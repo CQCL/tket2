6337f0b31f28b9c045fed27306f61eea6f31fde2
seyon <seyon.sivarajah@cambridgequantum.com>
2022-05-25 13:17:41 +0100

    fix bench script

--- benches/tket2_bench.rs
+++ benches/tket2_bench.rs
@@ -6,7 +6,7 @@ use tket_rs::{
         operation::Op,
     },
     graph::graph::PortIndex,
-    passes::pattern::PatternMatcher,
+    passes::pattern::{node_equality, FixedStructPattern, PatternMatcher},
 };
 
 fn pattern_match_bench_par(c: &mut Criterion) {
@@ -59,25 +59,22 @@ fn pattern_match_bench_par(c: &mut Criterion) {
                 .append_op(Op::H, &vec![PortIndex::new(1)])
                 .unwrap();
         }
-        let pmatcher = PatternMatcher::new(
-            target_circ.dag_ref(),
+        let pattern = FixedStructPattern::new(
             pattern_circ.dag_ref(),
             pattern_boundary,
+            node_equality(pattern_circ.dag_ref()),
         );
+        let pmatcher = PatternMatcher::new(pattern, target_circ.dag_ref());
 
         group.bench_function(BenchmarkId::new("Sequential", i), |b| {
             b.iter(|| {
-                let ms = pmatcher
-                    .find_matches(|x, y| pmatcher.node_equality(x, y))
-                    .collect::<Vec<_>>();
+                let ms = pmatcher.find_matches().collect::<Vec<_>>();
                 assert_eq!(ms.len(), i)
             })
         });
         group.bench_function(BenchmarkId::new("Paralllel", i), |b| {
             b.iter(|| {
-                let ms = pmatcher
-                    .find_par_matches(|x, y| pmatcher.node_equality(x, y))
-                    .collect::<Vec<_>>();
+                let ms = pmatcher.find_par_matches().collect::<Vec<_>>();
                 assert_eq!(ms.len(), i)
             })
         });
@@ -131,29 +128,7 @@ fn pattern_match_bench_recurse(c: &mut Criterion) {
         .append_op(Op::H, &vec![PortIndex::new(1)])
         .unwrap();
     let mut group = c.benchmark_group("PatternMatch");
-    for i in (0..1000).step_by(100) {
-        let pmatcher = PatternMatcher::new(
-            target_circ.dag_ref(),
-            pattern_circ.dag_ref(),
-            pattern_boundary,
-        );
-
-        group.bench_function(BenchmarkId::new("Recursive", i), |b| {
-            b.iter(|| {
-                let ms = pmatcher
-                    .find_matches_recurse(|x, y| pmatcher.node_equality(x, y))
-                    .collect::<Vec<_>>();
-                assert_eq!(ms.len(), 1)
-            })
-        });
-        group.bench_function(BenchmarkId::new("Iterative", i), |b| {
-            b.iter(|| {
-                let ms = pmatcher
-                    .find_matches(|x, y| pmatcher.node_equality(x, y))
-                    .collect::<Vec<_>>();
-                assert_eq!(ms.len(), 1)
-            })
-        });
+    for i in (100..1000).step_by(100) {
         for _ in 0..100 {
             target_circ
                 .append_op(Op::CX, &vec![PortIndex::new(0), PortIndex::new(1)])
@@ -175,6 +150,25 @@ fn pattern_match_bench_recurse(c: &mut Criterion) {
                 .append_op(Op::H, &vec![PortIndex::new(1)])
                 .unwrap();
         }
+        let pattern = FixedStructPattern::new(
+            pattern_circ.dag_ref(),
+            pattern_boundary,
+            node_equality(pattern_circ.dag_ref()),
+        );
+        let pmatcher = PatternMatcher::new(pattern, target_circ.dag_ref());
+
+        group.bench_function(BenchmarkId::new("Recursive", i), |b| {
+            b.iter(|| {
+                let ms = pmatcher.find_matches_recurse().collect::<Vec<_>>();
+                assert_eq!(ms.len(), 1)
+            })
+        });
+        group.bench_function(BenchmarkId::new("Iterative", i), |b| {
+            b.iter(|| {
+                let ms = pmatcher.find_matches().collect::<Vec<_>>();
+                assert_eq!(ms.len(), 1)
+            })
+        });
     }
     group.finish();
 }
