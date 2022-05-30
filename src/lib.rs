f5abdf7b7fd8a29bc9e5595d1a1e7ddb15eea6cc
seyon <seyon.sivarajah@cambridgequantum.com>
2022-05-30 11:41:06 +0100

    Param = f64

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,8 @@ mod tests {
     use crate::{
         circuit::{
             circuit::{Circuit, UnitID},
+            operation::WireType,
             operation::{ConstValue, Op},
-            operation::{Param, WireType},
         },
         passes::{
             apply_exhaustive,
@@ -23,12 +23,12 @@ mod tests {
     #[test]
     fn read_json() {
         // let expr = symengine::Expression::new("a + b + 3");
-        let circ_s = r#"{"bits": [["c", [0]], ["c", [1]]], "commands": [{"args": [["q", [0]]], "op": {"type": "H"}}, {"args": [["q", [0]], ["q", [1]]], "op": {"type": "CX"}}, {"args": [["q", [0]], ["c", [0]]], "op": {"type": "Measure"}}, {"args": [["q", [1]], ["c", [1]]], "op": {"type": "Measure"}}], "implicit_permutation": [[["q", [0]], ["q", [0]]], [["q", [1]], ["q", [1]]]], "phase": "0.0", "qubits": [["q", [0]], ["q", [1]]]}"#;
-        let ser: circuit_json::SerialCircuit<Param> = serde_json::from_str(circ_s).unwrap();
+        let circ_s = r#"{"bits": [["c", [0]], ["c", [1]]], "commands": [{"args": [["q", [0]]], "op": {"type": "H"}}, {"args": [["q", [0]], ["q", [1]]], "op": {"type": "CX"}}, {"args": [["q", [0]], ["c", [0]]], "op": {"type": "Measure"}}, {"args": [["q", [1]], ["c", [1]]], "op": {"type": "Measure"}}], "implicit_permutation": [[["q", [0]], ["q", [0]]], [["q", [1]], ["q", [1]]]], "phase": "0", "qubits": [["q", [0]], ["q", [1]]]}"#;
+        let ser: circuit_json::SerialCircuit = serde_json::from_str(circ_s).unwrap();
         assert_eq!(ser.commands.len(), 4);
 
         let circ: Circuit = ser.clone().into();
-        let _reser: SerialCircuit<Param> = circ.into();
+        let _reser: SerialCircuit = circ.into();
         assert_eq!(&ser, &_reser);
 
         // ser and reser cannot be compared because they will be different up to
