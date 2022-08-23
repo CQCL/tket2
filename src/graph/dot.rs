6e1054b3d600264e2fafff5719bc86e458a9a2c2
Lukas Heidemann <lukas@heidemann.me>
2022-08-23 21:13:25 +0200

    Draft of a graph using intrusive linked lists.

--- src/graph/dot.rs
+++ src/graph/dot.rs
@@ -1,6 +1,6 @@
 use std::fmt::Display;
 
-use super::graph::Graph;
+use super::graph::{Graph, Direction};
 
 pub fn dot_string<N: Display, E: Display>(graph: &Graph<N, E>) -> String {
     let mut s = String::new();
@@ -12,20 +12,25 @@ pub fn dot_string<N: Display, E: Display>(graph: &Graph<N, E>) -> String {
         s.push_str(&format!("{} [label=\"{:}\"]\n", n.index(), node)[..]);
     }
 
-    for e in graph.edge_indices() {
-        let [a, b] = graph.edge_endpoints(e).unwrap();
-        let edge = graph.edge_weight(e).unwrap();
-        s.push_str(
-            &format!(
-                "{} -> {} [label=\"({}, {}); {}\"]\n",
-                a.node.index(),
-                b.node.index(),
-                a.port.index(),
-                b.port.index(),
-                edge,
-            )[..],
-        );
-    }
+    // TODO: Needs to handle dangling edges
+
+    // for e in graph.edge_indices() {
+    //     let a = graph.edge_endpoins(e, Direction::Incoming);
+    //     let b = graph.edge_endpoint(e, Direction::Outgoing);
+
+
+    //     let edge = graph.edge_weight(e).unwrap();
+    //     s.push_str(
+    //         &format!(
+    //             "{} -> {} [label=\"({}, {}); {}\"]\n",
+    //             a.node.index(),
+    //             b.node.index(),
+    //             a.port.index(),
+    //             b.port.index(),
+    //             edge,
+    //         )[..],
+    //     );
+    // }
     s.push_str("}\n");
     s
 }
