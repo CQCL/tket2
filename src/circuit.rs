9fbee5701de320740b15431563043cb6cb7ad069
Agustin Borgna <agustin.borgna@quantinuum.com>
2023-06-28 16:22:28 +0100

    Rename `mod.rs`s, add docs warning

---
+++ src/circuit.rs
@@ -0,0 +1,12 @@
+#[allow(clippy::module_inception)]
+pub mod circuit;
+//pub mod dag;
+//pub mod operation;
+
+//#[cfg(feature = "pyo3")]
+//pub mod py_circuit;
+
+//#[cfg(feature = "tkcxx")]
+//pub mod unitarybox;
+
+//mod tk1ops;
