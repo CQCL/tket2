da2829646106f3fe2ed259a864382ea0be90c676
Agustin Borgna <agustin.borgna@quantinuum.com>
2023-07-20 00:00:57 +0100

    typo

--- src/circuit.rs
+++ src/circuit.rs
@@ -69,7 +69,7 @@ pub trait Circuit<'circ>: HugrView {
     /// Returns the input node to the circuit.
     fn input(&self) -> Node;
 
-    /// Returns the input node to the circuit.
+    /// Returns the output node to the circuit.
     fn output(&self) -> Node;
 }
 
