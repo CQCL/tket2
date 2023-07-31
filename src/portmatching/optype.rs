4efc5d746a3902f337162ec9f9eb2ae8f8fe68ce
Luca Mondada <luca@mondada.net>
2023-07-31 17:53:05 +0200

    Change ind -> name

--- src/portmatching/optype.rs
+++ src/portmatching/optype.rs
@@ -9,6 +9,7 @@
 use std::{hash::Hash, num::NonZeroU8};
 
 use hugr::ops::{LeafOp, OpType};
+use smol_str::SmolStr;
 
 /// A subset of LeafOp for pattern matching.
 ///
@@ -21,51 +22,51 @@ use hugr::ops::{LeafOp, OpType};
 pub struct MatchLeafOp(LeafOp);
 
 impl MatchLeafOp {
-    fn ind(&self) -> Option<NonZeroU8> {
+    fn id(&self) -> Option<SmolStr> {
         match self.0 {
-            LeafOp::H => NonZeroU8::new(1),
-            LeafOp::T => NonZeroU8::new(2),
-            LeafOp::S => NonZeroU8::new(3),
-            LeafOp::X => NonZeroU8::new(4),
-            LeafOp::Y => NonZeroU8::new(5),
-            LeafOp::Z => NonZeroU8::new(6),
-            LeafOp::Tadj => NonZeroU8::new(7),
-            LeafOp::Sadj => NonZeroU8::new(8),
-            LeafOp::CX => NonZeroU8::new(9),
-            LeafOp::ZZMax => NonZeroU8::new(10),
-            LeafOp::Measure => NonZeroU8::new(11),
-            LeafOp::RzF64 => NonZeroU8::new(12),
-            LeafOp::Xor => NonZeroU8::new(13),
+            LeafOp::H
+            | LeafOp::T
+            | LeafOp::S
+            | LeafOp::X
+            | LeafOp::Y
+            | LeafOp::Z
+            | LeafOp::Tadj
+            | LeafOp::Sadj
+            | LeafOp::CX
+            | LeafOp::ZZMax
+            | LeafOp::Measure
+            | LeafOp::RzF64
+            | LeafOp::Xor => Some(self.0.name()),
             _ => None,
         }
     }
 
-    fn ind_unchecked(&self) -> u8 {
-        self.ind().expect("Unsupported LeafOp").get()
+    fn id_unchecked(&self) -> SmolStr {
+        self.id().expect("Unsupported LeafOp")
     }
 }
 
 impl PartialEq for MatchLeafOp {
     fn eq(&self, other: &Self) -> bool {
-        self.ind_unchecked() == other.ind_unchecked()
+        self.id_unchecked() == other.id_unchecked()
     }
 }
 
 impl Hash for MatchLeafOp {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        state.write_u8(self.ind_unchecked())
+        state.write_u8(self.id_unchecked())
     }
 }
 
 impl PartialOrd for MatchLeafOp {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        self.ind_unchecked().partial_cmp(&other.ind_unchecked())
+        self.id_unchecked().partial_cmp(&other.id_unchecked())
     }
 }
 
 impl Ord for MatchLeafOp {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.ind().cmp(&other.ind())
+        self.id().cmp(&other.id())
     }
 }
 
@@ -74,7 +75,7 @@ impl TryFrom<LeafOp> for MatchLeafOp {
 
     fn try_from(value: LeafOp) -> Result<Self, Self::Error> {
         let value = MatchLeafOp(value);
-        value.ind().ok_or("Unsupported LeafOp")?;
+        value.id().ok_or("Unsupported LeafOp")?;
         Ok(value)
     }
 }
