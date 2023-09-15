edeaa6c6faa9b677119d9d465393c850f2952a5e
Agustin Borgna <agustin.borgna@quantinuum.com>
2023-09-15 14:07:01 +0100

    Move taso fns to TOptimiser methods

--- src/optimiser/taso.rs
+++ src/optimiser/taso.rs
@@ -119,23 +119,8 @@ where
         n_threads: NonZeroUsize,
     ) -> Hugr {
         match n_threads.get() {
-            1 => taso(
-                circ,
-                &self.rewriter,
-                &self.strategy,
-                &self.cost,
-                log_config,
-                timeout,
-            ),
-            _ => taso_mpsc(
-                circ,
-                &self.rewriter,
-                &self.strategy,
-                &self.cost,
-                log_config,
-                timeout,
-                n_threads,
-            ),
+            1 => self.taso(circ, log_config, timeout),
+            _ => self.taso_mpsc(circ, log_config, timeout, n_threads),
         }
     }
 
@@ -161,238 +146,217 @@ where
         let log_config = LogConfig::new(final_circ_json, circ_candidates_csv, progress_log);
         Ok(self.optimise_with_log(circ, log_config, timeout, n_threads))
     }
-}
 
-#[cfg(feature = "portmatching")]
-mod taso_default {
-    use crate::circuit::Circuit;
-    use crate::rewrite::strategy::ExhaustiveRewriteStrategy;
-    use crate::rewrite::ECCRewriter;
+    fn taso(&self, circ: &Hugr, mut log_config: LogConfig, timeout: Option<u64>) -> Hugr {
+        let start_time = Instant::now();
 
-    use super::*;
+        let mut log_candidates = log_config.circ_candidates_csv.map(csv::Writer::from_writer);
 
-    impl TasoOptimiser<ECCRewriter, ExhaustiveRewriteStrategy, fn(&Hugr) -> usize> {
-        /// A sane default optimiser using the given ECC sets.
-        pub fn default_with_eccs_json_file(eccs_path: impl AsRef<std::path::Path>) -> Self {
-            let rewriter = ECCRewriter::from_eccs_json_file(eccs_path);
-            let strategy = ExhaustiveRewriteStrategy::default();
-            Self::new(rewriter, strategy, |c| c.num_gates())
-        }
-    }
-}
+        let mut best_circ = circ.clone();
+        let mut best_circ_cost = (self.cost)(circ);
+        log_best(best_circ_cost, log_candidates.as_mut()).unwrap();
 
-fn taso(
-    circ: &Hugr,
-    rewriter: &impl Rewriter,
-    strategy: &impl RewriteStrategy,
-    cost: impl Fn(&Hugr) -> usize,
-    mut log_config: LogConfig,
-    timeout: Option<u64>,
-) -> Hugr {
-    let start_time = Instant::now();
+        // Hash of seen circuits. Dot not store circuits as this map gets huge
+        let mut seen_hashes: FxHashSet<_> = FromIterator::from_iter([(circ.circuit_hash())]);
 
-    let mut log_candidates = log_config.circ_candidates_csv.map(csv::Writer::from_writer);
+        // The priority queue of circuits to be processed (this should not get big)
+        let mut pq = HugrPQ::new(&self.cost);
 
-    let mut best_circ = circ.clone();
-    let mut best_circ_cost = cost(circ);
-    log_best(best_circ_cost, log_candidates.as_mut()).unwrap();
+        pq.push(circ.clone());
 
-    // Hash of seen circuits. Dot not store circuits as this map gets huge
-    let mut seen_hashes: FxHashSet<_> = FromIterator::from_iter([(circ.circuit_hash())]);
-
-    // The priority queue of circuits to be processed (this should not get big)
-    let mut pq = HugrPQ::new(&cost);
-
-    pq.push(circ.clone());
-
-    let mut circ_cnt = 0;
-    while let Some(Entry { circ, cost, .. }) = pq.pop() {
-        if cost < best_circ_cost {
-            best_circ = circ.clone();
-            best_circ_cost = cost;
-            log_best(best_circ_cost, log_candidates.as_mut()).unwrap();
-        }
-
-        let rewrites = rewriter.get_rewrites(&circ);
-        for new_circ in strategy.apply_rewrites(rewrites, &circ) {
-            let new_circ_hash = new_circ.circuit_hash();
-            circ_cnt += 1;
-            if circ_cnt % 1000 == 0 {
-                log_progress(
-                    log_config.progress_log.as_mut(),
-                    circ_cnt,
-                    &pq,
-                    &seen_hashes,
-                )
-                .expect("Failed to write to progress log");
-            }
-            if seen_hashes.contains(&new_circ_hash) {
-                continue;
-            }
-            pq.push_with_hash_unchecked(new_circ, new_circ_hash);
-            seen_hashes.insert(new_circ_hash);
-        }
-
-        if pq.len() >= 10000 {
-            // Haircut to keep the queue size manageable
-            pq.truncate(5000);
-        }
-
-        if let Some(timeout) = timeout {
-            if start_time.elapsed().as_secs() > timeout {
-                println!("Timeout");
-                break;
-            }
-        }
-    }
-
-    log_final(
-        &best_circ,
-        log_config.progress_log.as_mut(),
-        log_config.final_circ_json.as_mut(),
-        &cost,
-    )
-    .expect("Failed to write to progress log and/or final circuit JSON");
-
-    best_circ
-}
-
-/// Run the TASO optimiser on a circuit, using multiple threads.
-///
-/// This is the multi-threaded version of [`taso`]. See [`TasoOptimiser`] for
-/// more details.
-fn taso_mpsc(
-    circ: &Hugr,
-    rewriter: &(impl Rewriter + Send + Clone + 'static),
-    strategy: &(impl RewriteStrategy + Send + Clone + 'static),
-    cost: impl Fn(&Hugr) -> usize + Send + Sync,
-    log_config: LogConfig,
-    timeout: Option<u64>,
-    n_threads: NonZeroUsize,
-) -> Hugr {
-    let n_threads: usize = n_threads.get();
-    let start_time = Instant::now();
-
-    let mut log_candidates = log_config.circ_candidates_csv.map(csv::Writer::from_writer);
-
-    println!("Spinning up {n_threads} threads");
-
-    // channel for sending circuits from threads back to main
-    let (t_main, r_main) = mpsc::sync_channel(n_threads * 100);
-
-    let mut best_circ = circ.clone();
-    let mut best_circ_cost = cost(&best_circ);
-    let circ_hash = circ.circuit_hash();
-    log_best(best_circ_cost, log_candidates.as_mut()).unwrap();
-
-    // Hash of seen circuits. Dot not store circuits as this map gets huge
-    let mut seen_hashes: FxHashSet<_> = FromIterator::from_iter([(circ_hash)]);
-
-    // The priority queue of circuits to be processed (this should not get big)
-    let mut pq = HugrPQ::new(&cost);
-    pq.push(circ.clone());
-
-    // each thread scans for rewrites using all the patterns and
-    // sends rewritten circuits back to main
-    let (joins, threads_tx, signal_new_data): (Vec<_>, Vec<_>, Vec<_>) = (0..n_threads)
-        .map(|_| spawn_pattern_matching_thread(t_main.clone(), rewriter.clone(), strategy.clone()))
-        .multiunzip();
-
-    let mut cycle_inds = (0..n_threads).cycle();
-    let mut threads_empty = vec![true; n_threads];
-
-    let mut circ_cnt = 0;
-    loop {
-        // Fill each thread workqueue with data from pq
-        while let Some(Entry {
-            circ,
-            cost: &cost,
-            hash,
-        }) = pq.peek()
-        {
+        let mut circ_cnt = 0;
+        while let Some(Entry { circ, cost, .. }) = pq.pop() {
             if cost < best_circ_cost {
                 best_circ = circ.clone();
                 best_circ_cost = cost;
                 log_best(best_circ_cost, log_candidates.as_mut()).unwrap();
-                // Now we only care about smaller circuits
-                seen_hashes.clear();
-                seen_hashes.insert(hash);
             }
-            // try to send to first available thread
-            // TODO: Consider using crossbeam-channel
-            if let Some(next_ind) = cycle_inds.by_ref().take(n_threads).find(|next_ind| {
-                let tx = &threads_tx[*next_ind];
-                tx.try_send(Some(circ.clone())).is_ok()
-            }) {
-                pq.pop();
-                // Unblock thread if waiting
-                let _ = signal_new_data[next_ind].try_recv();
-                threads_empty[next_ind] = false;
-            } else {
-                // All send channels are full, continue
-                break;
+
+            let rewrites = self.rewriter.get_rewrites(&circ);
+            for new_circ in self.strategy.apply_rewrites(rewrites, &circ) {
+                let new_circ_hash = new_circ.circuit_hash();
+                circ_cnt += 1;
+                if circ_cnt % 1000 == 0 {
+                    log_progress(
+                        log_config.progress_log.as_mut(),
+                        circ_cnt,
+                        &pq,
+                        &seen_hashes,
+                    )
+                    .expect("Failed to write to progress log");
+                }
+                if seen_hashes.contains(&new_circ_hash) {
+                    continue;
+                }
+                pq.push_with_hash_unchecked(new_circ, new_circ_hash);
+                seen_hashes.insert(new_circ_hash);
+            }
+
+            if pq.len() >= 10000 {
+                // Haircut to keep the queue size manageable
+                pq.truncate(5000);
+            }
+
+            if let Some(timeout) = timeout {
+                if start_time.elapsed().as_secs() > timeout {
+                    println!("Timeout");
+                    break;
+                }
             }
         }
 
-        // Receive data from threads, add to pq
-        // We compute the hashes in the threads because it's expensive
-        while let Ok(received) = r_main.try_recv() {
-            let Some((circ_hash, circ)) = received else {
-                panic!("A thread panicked");
-            };
-            circ_cnt += 1;
-            if circ_cnt % 1000 == 0 {
-                println!("{circ_cnt} circuits...");
-                println!("Queue size: {} circuits", pq.len());
-                println!("Total seen: {} circuits", seen_hashes.len());
-            }
-            if seen_hashes.contains(&circ_hash) {
-                continue;
-            }
-            pq.push_with_hash_unchecked(circ, circ_hash);
-            seen_hashes.insert(circ_hash);
-        }
+        log_final(
+            &best_circ,
+            log_config.progress_log.as_mut(),
+            log_config.final_circ_json.as_mut(),
+            &self.cost,
+        )
+        .expect("Failed to write to progress log and/or final circuit JSON");
 
-        // Check if all threads are waiting for new data
-        for (is_waiting, is_empty) in signal_new_data.iter().zip(threads_empty.iter_mut()) {
-            if is_waiting.try_recv().is_ok() {
-                *is_empty = true;
-            }
-        }
-        // If everyone is waiting and we do not have new data, we are done
-        if pq.is_empty() && threads_empty.iter().all(|&x| x) {
-            break;
-        }
-        if let Some(timeout) = timeout {
-            if start_time.elapsed().as_secs() > timeout {
-                println!("Timeout");
-                break;
-            }
-        }
-        if pq.len() >= 10000 {
-            // Haircut to keep the queue size manageable
-            pq.truncate(5000);
-        }
+        best_circ
     }
 
-    println!("Tried {circ_cnt} circuits");
-    println!("Joining");
+    /// Run the TASO optimiser on a circuit, using multiple threads.
+    ///
+    /// This is the multi-threaded version of [`taso`]. See [`TasoOptimiser`] for
+    /// more details.
+    fn taso_mpsc(
+        &self,
+        circ: &Hugr,
+        log_config: LogConfig,
+        timeout: Option<u64>,
+        n_threads: NonZeroUsize,
+    ) -> Hugr {
+        let n_threads: usize = n_threads.get();
+        let start_time = Instant::now();
 
-    for (join, tx, data_tx) in izip!(joins, threads_tx, signal_new_data) {
-        // tell all the threads we're done and join the threads
-        tx.send(None).unwrap();
-        let _ = data_tx.try_recv();
-        join.join().unwrap();
+        let mut log_candidates = log_config.circ_candidates_csv.map(csv::Writer::from_writer);
+
+        println!("Spinning up {n_threads} threads");
+
+        // channel for sending circuits from threads back to main
+        let (t_main, r_main) = mpsc::sync_channel(n_threads * 100);
+
+        let mut best_circ = circ.clone();
+        let mut best_circ_cost = (self.cost)(&best_circ);
+        let circ_hash = circ.circuit_hash();
+        log_best(best_circ_cost, log_candidates.as_mut()).unwrap();
+
+        // Hash of seen circuits. Dot not store circuits as this map gets huge
+        let mut seen_hashes: FxHashSet<_> = FromIterator::from_iter([(circ_hash)]);
+
+        // The priority queue of circuits to be processed (this should not get big)
+        let mut pq = HugrPQ::new(&self.cost);
+        pq.push(circ.clone());
+
+        // each thread scans for rewrites using all the patterns and
+        // sends rewritten circuits back to main
+        let (joins, threads_tx, signal_new_data): (Vec<_>, Vec<_>, Vec<_>) = (0..n_threads)
+            .map(|_| {
+                spawn_pattern_matching_thread(
+                    t_main.clone(),
+                    self.rewriter.clone(),
+                    self.strategy.clone(),
+                )
+            })
+            .multiunzip();
+
+        let mut cycle_inds = (0..n_threads).cycle();
+        let mut threads_empty = vec![true; n_threads];
+
+        let mut circ_cnt = 0;
+        loop {
+            // Fill each thread workqueue with data from pq
+            while let Some(Entry {
+                circ,
+                cost: &cost,
+                hash,
+            }) = pq.peek()
+            {
+                if cost < best_circ_cost {
+                    best_circ = circ.clone();
+                    best_circ_cost = cost;
+                    log_best(best_circ_cost, log_candidates.as_mut()).unwrap();
+                    // Now we only care about smaller circuits
+                    seen_hashes.clear();
+                    seen_hashes.insert(hash);
+                }
+                // try to send to first available thread
+                // TODO: Consider using crossbeam-channel
+                if let Some(next_ind) = cycle_inds.by_ref().take(n_threads).find(|next_ind| {
+                    let tx = &threads_tx[*next_ind];
+                    tx.try_send(Some(circ.clone())).is_ok()
+                }) {
+                    pq.pop();
+                    // Unblock thread if waiting
+                    let _ = signal_new_data[next_ind].try_recv();
+                    threads_empty[next_ind] = false;
+                } else {
+                    // All send channels are full, continue
+                    break;
+                }
+            }
+
+            // Receive data from threads, add to pq
+            // We compute the hashes in the threads because it's expensive
+            while let Ok(received) = r_main.try_recv() {
+                let Some((circ_hash, circ)) = received else {
+                    panic!("A thread panicked");
+                };
+                circ_cnt += 1;
+                if circ_cnt % 1000 == 0 {
+                    println!("{circ_cnt} circuits...");
+                    println!("Queue size: {} circuits", pq.len());
+                    println!("Total seen: {} circuits", seen_hashes.len());
+                }
+                if seen_hashes.contains(&circ_hash) {
+                    continue;
+                }
+                pq.push_with_hash_unchecked(circ, circ_hash);
+                seen_hashes.insert(circ_hash);
+            }
+
+            // Check if all threads are waiting for new data
+            for (is_waiting, is_empty) in signal_new_data.iter().zip(threads_empty.iter_mut()) {
+                if is_waiting.try_recv().is_ok() {
+                    *is_empty = true;
+                }
+            }
+            // If everyone is waiting and we do not have new data, we are done
+            if pq.is_empty() && threads_empty.iter().all(|&x| x) {
+                break;
+            }
+            if let Some(timeout) = timeout {
+                if start_time.elapsed().as_secs() > timeout {
+                    println!("Timeout");
+                    break;
+                }
+            }
+            if pq.len() >= 10000 {
+                // Haircut to keep the queue size manageable
+                pq.truncate(5000);
+            }
+        }
+
+        println!("Tried {circ_cnt} circuits");
+        println!("Joining");
+
+        for (join, tx, data_tx) in izip!(joins, threads_tx, signal_new_data) {
+            // tell all the threads we're done and join the threads
+            tx.send(None).unwrap();
+            let _ = data_tx.try_recv();
+            join.join().unwrap();
+        }
+
+        println!("END RESULT: {}", (self.cost)(&best_circ));
+        fs::write("final_best_circ.gv", best_circ.dot_string()).unwrap();
+        fs::write(
+            "final_best_circ.json",
+            serde_json::to_vec(&best_circ).unwrap(),
+        )
+        .unwrap();
+        best_circ
     }
-
-    println!("END RESULT: {}", cost(&best_circ));
-    fs::write("final_best_circ.gv", best_circ.dot_string()).unwrap();
-    fs::write(
-        "final_best_circ.json",
-        serde_json::to_vec(&best_circ).unwrap(),
-    )
-    .unwrap();
-    best_circ
 }
 
 fn spawn_pattern_matching_thread(
@@ -425,6 +389,24 @@ fn spawn_pattern_matching_thread(
     });
 
     (jn, tx_thread, signal_new_data)
+}
+
+#[cfg(feature = "portmatching")]
+mod taso_default {
+    use crate::circuit::Circuit;
+    use crate::rewrite::strategy::ExhaustiveRewriteStrategy;
+    use crate::rewrite::ECCRewriter;
+
+    use super::*;
+
+    impl TasoOptimiser<ECCRewriter, ExhaustiveRewriteStrategy, fn(&Hugr) -> usize> {
+        /// A sane default optimiser using the given ECC sets.
+        pub fn default_with_eccs_json_file(eccs_path: impl AsRef<std::path::Path>) -> Self {
+            let rewriter = ECCRewriter::from_eccs_json_file(eccs_path);
+            let strategy = ExhaustiveRewriteStrategy::default();
+            Self::new(rewriter, strategy, |c| c.num_gates())
+        }
+    }
 }
 
 /// A helper struct for logging improvements in circuit size seen during the
