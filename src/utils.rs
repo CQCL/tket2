c026a5f223793f32926dff3fafdd4ec73a7f5a2e
Agustin Borgna <agustin.borgna@quantinuum.com>
2023-10-02 22:54:57 +0100

    chore: Small code cleanups
    - Remove duplicated tk1 writer fn
    - Better .dot debug API

--- src/utils.rs
+++ src/utils.rs
@@ -36,13 +36,22 @@ pub(crate) fn build_simple_circuit(
 #[allow(dead_code)]
 #[cfg(test)]
 pub(crate) mod test {
+    #[allow(unused_imports)]
+    use hugr::HugrView;
+
     /// Open a browser page to render a dot string graph.
     ///
     /// This can be used directly on the output of `Hugr::dot_string`
     #[cfg(not(ci_run))]
-    pub(crate) fn viz_dotstr(dotstr: &str) {
+    pub(crate) fn viz_dotstr(dotstr: impl AsRef<str>) {
         let mut base: String = "https://dreampuf.github.io/GraphvizOnline/#".into();
-        base.push_str(&urlencoding::encode(dotstr));
+        base.push_str(&urlencoding::encode(dotstr.as_ref()));
         webbrowser::open(&base).unwrap();
+    }
+
+    /// Open a browser page to render a HugrView's dot string graph.
+    #[cfg(not(ci_run))]
+    pub(crate) fn viz_hugr(hugr: &impl HugrView) {
+        viz_dotstr(hugr.dot_string());
     }
 }
