91f8d08b8b9a8737ef9309d1e9d539ca244a1d56
Luca Mondada <luca@mondada.net>
2023-10-04 14:29:34 +0200

    Further spread JSON

--- tests/taso_termination.rs
+++ tests/taso_termination.rs
@@ -32,10 +32,23 @@ mod require_portmatching {
         // The TK1 json of the circuit
         let json = r#"{
             "bits": [],
-            "commands": [{"args": [["q", [0]], ["q", [2]]], "op": {"type": "CX"}}, {"args": [["q", [0]]], "op": {"params": ["0.1"], "type": "Rz"}}, {"args": [["q", [0]], ["q", [1]]], "op": {"type": "CX"}}, {"args": [["q", [1]]], "op": {"type": "H"}}, {"args": [["q", [1]]], "op": {"params": ["0.2"], "type": "Rz"}}, {"args": [["q", [1]]], "op": {"type": "H"}}, {"args": [["q", [0]], ["q", [1]]], "op": {"type": "CX"}}, {"args": [["q", [0]], ["q", [2]]], "op": {"type": "CX"}}, {"args": [["q", [0]]], "op": {"params": ["-0.1"], "type": "Rz"}}],
+            "commands": [
+                {"args": [["q", [0]], ["q", [2]]], "op": {"type": "CX"}},
+                {"args": [["q", [0]]], "op": {"params": ["0.1"], "type": "Rz"}},
+                {"args": [["q", [0]], ["q", [1]]], "op": {"type": "CX"}},
+                {"args": [["q", [1]]], "op": {"type": "H"}},
+                {"args": [["q", [1]]], "op": {"params": ["0.2"], "type": "Rz"}},
+                {"args": [["q", [1]]], "op": {"type": "H"}},
+                {"args": [["q", [0]], ["q", [1]]], "op": {"type": "CX"}},
+                {"args": [["q", [0]], ["q", [2]]], "op": {"type": "CX"}},
+                {"args": [["q", [0]]], "op": {"params": ["-0.1"], "type": "Rz"}}],
             "created_qubits": [],
             "discarded_qubits": [],
-            "implicit_permutation": [[["q", [0]], ["q", [0]]], [["q", [1]], ["q", [1]]], [["q", [2]], ["q", [2]]]], "phase": "0.0", "qubits": [["q", [0]], ["q", [1]], ["q", [2]]]
+            "implicit_permutation": [
+                [["q", [0]], ["q", [0]]], [["q", [1]], ["q", [1]]], [["q", [2]], ["q", [2]]]
+            ],
+            "phase": "0.0",
+            "qubits": [["q", [0]], ["q", [1]], ["q", [2]]]
         }"#;
         let ser: SerialCircuit = serde_json::from_str(json).unwrap();
         ser.decode().unwrap()
