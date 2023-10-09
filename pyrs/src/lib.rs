507dd95a0c9071d46ad49a901fc9f26909926d48
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2023-10-09 14:04:21 +0200

    feat: high(-er) level rule, find, apply API
    Only returns first found match for now

--- pyrs/src/lib.rs
+++ pyrs/src/lib.rs
@@ -1,13 +1,87 @@
 //! Python bindings for TKET2.
 #![warn(missing_docs)]
-use circuit::{add_circuit_module, try_with_hugr};
+use circuit::{add_circuit_module, to_hugr, try_with_hugr};
+use hugr::Hugr;
 use optimiser::add_optimiser_module;
 use pyo3::prelude::*;
-use tket2::{json::TKETDecode, passes::apply_greedy_commutation};
+use tket2::{
+    json::TKETDecode,
+    passes::apply_greedy_commutation,
+    portmatching::{pyo3::PyPatternMatch, CircuitPattern, PatternMatcher},
+    rewrite::CircuitRewrite,
+};
 use tket_json_rs::circuit_json::SerialCircuit;
 
 mod circuit;
 mod optimiser;
+
+#[derive(Clone)]
+#[pyclass]
+/// A rewrite rule defined by a left hand side and right hand side of an equation.
+pub struct Rule(pub [Hugr; 2]);
+
+#[pymethods]
+impl Rule {
+    #[new]
+    fn new_rule(l: PyObject, r: PyObject) -> PyResult<Rule> {
+        let l = to_hugr(l)?;
+        let r = to_hugr(r)?;
+
+        Ok(Rule([l, r]))
+    }
+}
+#[pyclass]
+struct RuleMatcher {
+    matcher: PatternMatcher,
+    rights: Vec<Hugr>,
+}
+
+#[pymethods]
+impl RuleMatcher {
+    #[new]
+    pub fn from_rules(rules: Vec<Rule>) -> PyResult<Self> {
+        let (lefts, rights): (Vec<_>, Vec<_>) =
+            rules.into_iter().map(|Rule([l, r])| (l, r)).unzip();
+        let patterns: Result<Vec<CircuitPattern>, _> =
+            lefts.iter().map(CircuitPattern::try_from_circuit).collect();
+        let matcher = PatternMatcher::from_patterns(patterns?);
+
+        Ok(Self { matcher, rights })
+    }
+
+    pub fn find_match(&self, target: &T2Circuit) -> PyResult<Option<CircuitRewrite>> {
+        let h = &target.0;
+        let p_match = self.matcher.find_matches_iter(h).next();
+        if let Some(m) = p_match {
+            let py_match = PyPatternMatch::try_from_rust(m, h, &self.matcher)?;
+            let r = self.rights.get(py_match.pattern_id).unwrap().clone();
+            let rw = py_match.to_rewrite(h, r)?;
+            Ok(Some(rw))
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+#[pyclass]
+/// A manager for tket 2 operations on a tket 1 Circuit.
+pub struct T2Circuit(Hugr);
+
+#[pymethods]
+impl T2Circuit {
+    #[new]
+    fn from_circuit(circ: PyObject) -> PyResult<Self> {
+        Ok(Self(to_hugr(circ)?))
+    }
+
+    fn finish(&self) -> PyResult<PyObject> {
+        SerialCircuit::encode(&self.0)?.to_tket1()
+    }
+
+    fn apply_match(&mut self, rw: CircuitRewrite) {
+        rw.apply(&mut self.0).expect("Apply error.");
+    }
+}
 
 #[pyfunction]
 fn greedy_depth_reduce(py_c: PyObject) -> PyResult<(PyObject, u32)> {
@@ -25,6 +99,9 @@ fn pyrs(py: Python, m: &PyModule) -> PyResult<()> {
     add_pattern_module(py, m)?;
     add_pass_module(py, m)?;
     add_optimiser_module(py, m)?;
+    m.add_class::<Rule>()?;
+    m.add_class::<RuleMatcher>()?;
+    m.add_class::<T2Circuit>()?;
     Ok(())
 }
 
