fadb2c49d9d701325f00b8a2af717cec2d901a56
Luca Mondada <72734770+lmondada@users.noreply.github.com>
2023-10-10 17:17:49 +0200

    feat: Add Python TASO pass (#180)
    This is a useful Python binding to create the pytket-hyper extension on
    top of.

--- pyrs/src/lib.rs
+++ pyrs/src/lib.rs
@@ -1,19 +1,20 @@
 //! Python bindings for TKET2.
 #![warn(missing_docs)]
-use circuit::{add_circuit_module, to_hugr, try_with_hugr, T2Circuit};
-use hugr::Hugr;
-use optimiser::add_optimiser_module;
-use pyo3::prelude::*;
-use tket2::{
-    json::TKETDecode,
-    passes::apply_greedy_commutation,
-    portmatching::{pyo3::PyPatternMatch, CircuitPattern, PatternMatcher},
-    rewrite::CircuitRewrite,
-};
-use tket_json_rs::circuit_json::SerialCircuit;
 
 mod circuit;
 mod optimiser;
+mod pass;
+
+use circuit::{add_circuit_module, to_hugr, T2Circuit};
+use optimiser::add_optimiser_module;
+use pass::add_pass_module;
+
+use hugr::Hugr;
+use pyo3::prelude::*;
+use tket2::{
+    portmatching::{pyo3::PyPatternMatch, CircuitPattern, PatternMatcher},
+    rewrite::CircuitRewrite,
+};
 
 #[derive(Clone)]
 #[pyclass]
@@ -63,15 +64,6 @@ impl RuleMatcher {
     }
 }
 
-#[pyfunction]
-fn greedy_depth_reduce(py_c: PyObject) -> PyResult<(PyObject, u32)> {
-    try_with_hugr(py_c, |mut h| {
-        let n_moves = apply_greedy_commutation(&mut h)?;
-        let py_c = SerialCircuit::encode(&h)?.to_tket1()?;
-        PyResult::Ok((py_c, n_moves))
-    })
-}
-
 /// The Python bindings to TKET2.
 #[pymodule]
 fn pyrs(py: Python, m: &PyModule) -> PyResult<()> {
@@ -101,16 +93,4 @@ fn add_pattern_module(py: Python, parent: &PyModule) -> PyResult<()> {
     )?;
 
     parent.add_submodule(m)
-}
-
-fn add_pass_module(py: Python, parent: &PyModule) -> PyResult<()> {
-    let m = PyModule::new(py, "passes")?;
-    m.add_function(wrap_pyfunction!(greedy_depth_reduce, m)?)?;
-    m.add_class::<tket2::T2Op>()?;
-    m.add(
-        "PullForwardError",
-        py.get_type::<tket2::passes::PyPullForwardError>(),
-    )?;
-    parent.add_submodule(m)?;
-    Ok(())
 }
