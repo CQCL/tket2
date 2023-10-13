aaf2badea6397f23697c29d9314d10c1bbfd8a5a
Luca Mondada <luca@mondada.net>
2023-10-13 07:43:09 +0200

    fix test (apply mutates input obv)

--- pyrs/test/test_pass.py
+++ pyrs/test/test_pass.py
@@ -5,5 +5,5 @@ from pyrs.passes import taso_pass
 def test_simple_taso_pass_no_opt():
     c = Circuit(3).CCX(0, 1, 2)
     taso = taso_pass(max_threads = 1, timeout = 0)
-    c = taso.apply(c)
+    taso.apply(c)
     assert c.n_gates_of_type(OpType.CX) == 6
