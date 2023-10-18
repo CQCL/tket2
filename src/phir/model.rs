0f9bc6f28020d1accf9b57ebb78f77ed2f99a128
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2023-10-18 15:03:48 +0100

    break up model enums

--- src/phir/model.rs
+++ src/phir/model.rs
@@ -1,10 +1,11 @@
+use derive_more::From;
 use serde::{Deserialize, Serialize};
 use serde_json::{Map, Value};
 
 type Metadata = Option<Map<String, Value>>;
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
-struct Data {
+pub struct Data {
     #[serde(flatten)]
     data: DataEnum,
     #[serde(skip_serializing_if = "Option::is_none")]
@@ -30,35 +31,43 @@ fn default_version() -> String {
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
-#[serde(tag = "data")]
-enum DataEnum {
-    #[serde(rename = "cvar_define")]
-    CVarDefine {
-        #[serde(default = "default_cvar_def_data")]
-        data_type: String,
-        variable: String,
-        #[serde(skip_serializing_if = "Option::is_none")]
-        size: Option<u64>,
-    },
-    #[serde(rename = "qvar_define")]
-    QVarDefine {
-        #[serde(default = "default_qvar_def_data")]
-        data_type: Option<String>,
-        variable: String,
-        size: u64,
-    },
-
-    #[serde(rename = "cvar_export")]
-    ExportVar {
-        variables: Vec<String>,
-        #[serde(skip_serializing_if = "Option::is_none")]
-        to: Option<Vec<String>>,
-    },
+pub(super) struct CVarDefine {
+    #[serde(default = "default_cvar_def_data")]
+    pub(super) data_type: String,
+    pub(super) variable: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(super) size: Option<u64>,
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub(super) struct QVarDefine {
+    #[serde(default = "default_qvar_def_data")]
+    pub(super) data_type: Option<String>,
+    pub(super) variable: String,
+    pub(super) size: u64,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub(super) struct ExportVar {
+    pub(super) variables: Vec<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(super) to: Option<Vec<String>>,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, From)]
+#[serde(tag = "data")]
+pub(super) enum DataEnum {
+    #[serde(rename = "cvar_define")]
+    CVarDefine(CVarDefine),
+    #[serde(rename = "qvar_define")]
+    QVarDefine(QVarDefine),
+    #[serde(rename = "cvar_export")]
+    ExportVar(ExportVar),
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, From)]
 #[serde(untagged)]
-enum Arg {
+pub(super) enum Arg {
     Register(String),
     Number(u64),
     RegIndex((String, u64)),
@@ -66,84 +75,101 @@ enum Arg {
     Other(Value),
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, From)]
 #[serde(untagged)]
-enum CopArg {
+pub(super) enum CopArg {
     Arg(Arg),
     Cop(Cop),
 }
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
-struct Op {
+pub(super) struct Op {
     #[serde(flatten)]
-    op_enum: OpEnum,
+    pub op_enum: OpEnum,
     #[serde(skip_serializing_if = "Option::is_none")]
-    returns: Option<Vec<Arg>>,
+    pub returns: Option<Vec<Arg>>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    metadata: Metadata,
+    pub metadata: Metadata,
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
-struct Cop {
-    cop: String,
-    args: Vec<CopArg>,
+pub(super) struct Cop {
+    pub cop: String,
+    pub args: Vec<CopArg>,
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub(super) struct Qop {
+    pub qop: String,
+    pub args: Vec<Arg>,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub(super) struct FFCall {
+    #[serde(default = "default_ffcall_cop")]
+    pub cop: String,
+    pub function: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub(super) struct Mop {
+    pub mop: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, From)]
 #[serde(untagged)]
-enum OpEnum {
-    Qop {
-        qop: String,
-        args: Vec<Arg>,
-    },
+pub(super) enum OpEnum {
+    Qop(Qop),
     Cop(Cop),
-    FFCall {
-        #[serde(default = "default_ffcall_cop")]
-        cop: String,
-        function: String,
-    },
-    Mop {
-        mop: String,
-    },
+    FFCall(FFCall),
+    Mop(Mop),
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
-struct Block {
+pub(super) struct Block {
     #[serde(flatten)]
-    block_enum: BlockEnum,
+    pub block_enum: BlockEnum,
     #[serde(skip_serializing_if = "Option::is_none")]
-    metadata: Option<Map<String, Value>>,
+    pub metadata: Option<Map<String, Value>>,
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub(super) struct If {
+    pub(super) condition: Cop,
+    pub(super) true_branch: Vec<Op>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(super) false_branch: Option<Vec<Op>>,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub(super) struct Seq {
+    pub(super) ops: Vec<BlockElems>,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, From)]
 #[serde(tag = "block")]
-enum BlockEnum {
+pub(super) enum BlockEnum {
     #[serde(rename = "sequence")]
-    Seq { ops: Vec<BlockElems> },
+    Seq(Seq),
     #[serde(rename = "if")]
-    If {
-        condition: Cop,
-        true_branch: Vec<Op>,
-        #[serde(skip_serializing_if = "Option::is_none")]
-        false_branch: Option<Vec<Op>>,
-    },
+    If(If),
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, From)]
 #[serde(untagged)]
-enum BlockElems {
+pub(super) enum BlockElems {
     Op(Op),
     Block(Block),
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
-struct Comment {
+pub(super) struct Comment {
     #[serde(rename = "//")]
     c: String,
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, From)]
 #[serde(untagged)]
-enum OpListElems {
+pub(super) enum OpListElems {
     Comment(Comment),
     Op(Op),
     Block(Block),
@@ -159,6 +185,29 @@ pub struct PHIRModel {
     #[serde(skip_serializing_if = "Option::is_none")]
     metadata: Metadata,
     ops: Vec<OpListElems>,
+}
+
+impl PHIRModel {
+    /// Creates a new [`PHIRModel`].
+    pub fn new() -> Self {
+        Self {
+            format: default_format(),
+            version: default_version(),
+            metadata: None,
+            ops: vec![],
+        }
+    }
+
+    /// .
+    pub(super) fn add_op(&mut self, op: impl Into<OpListElems>) {
+        self.ops.push(op.into());
+    }
+}
+
+impl Default for PHIRModel {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 #[cfg(test)]
