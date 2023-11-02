eee99cb875dab4c920092c94acca4ad8284540c7
Agustin Borgna <agustin.borgna@quantinuum.com>
2023-11-02 15:15:24 +0000

    Update paths in CI

--- pytket-tk2/pytket_tk2/__init__.py
+++ pytket-tk2/pytket_tk2/__init__.py
@@ -1,5 +1,5 @@
-from .pytket_tk2 import *
+from .pytket_tk2 import *  # noqa: F403,F405
 
-__doc__ = pytket_tk2.__doc__
-if hasattr(pytket_tk2, "__all__"):
-    __all__ = pytket_tk2.__all__
+__doc__ = pytket_tk2.__doc__  # noqa: F405
+if hasattr(pytket_tk2, "__all__"):  # noqa: F405
+    __all__ = pytket_tk2.__all__  # noqa: F405
