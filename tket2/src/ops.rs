9332b6e4b1cbe5df775b7cbc3cf2610de2161d45
Agustín Borgna <121866228+aborgna-q@users.noreply.github.com>
2023-12-15 17:50:15 +0100

    fix: Encode opaque symbolic constants (#273)
    Opaque ops weren't being decoded.

--- tket2/src/ops.rs
+++ tket2/src/ops.rs
@@ -183,25 +183,30 @@ pub fn symbolic_constant_op(s: &str) -> OpType {
 }
 
 /// match against a symbolic constant
-pub(crate) fn match_symb_const_op(op: &OpType) -> Option<&str> {
+pub(crate) fn match_symb_const_op(op: &OpType) -> Option<String> {
+    // Extract the symbol for a symbolic operation node.
+    let symbol_from_typeargs = |args: &[TypeArg]| -> String {
+        args.first()
+            .and_then(|arg| match arg {
+                TypeArg::Opaque { arg } => match &arg.value {
+                    serde_yaml::Value::String(s) => Some(s.clone()),
+                    _ => None,
+                },
+                _ => None,
+            })
+            .unwrap_or_else(|| panic!("Found an invalid type arg in a symbolic operation node."))
+    };
+
     if let OpType::LeafOp(LeafOp::CustomOp(e)) = op {
         match e.as_ref() {
             ExternalOp::Extension(e)
                 if e.def().name() == &SYM_OP_ID && e.def().extension() == &EXTENSION_ID =>
             {
-                // TODO also check extension name
-
-                let Some(TypeArg::Opaque { arg }) = e.args().first() else {
-                    panic!("should be an opaque type arg.")
-                };
-
-                let serde_yaml::Value::String(s) = &arg.value else {
-                    panic!("unexpected yaml value.")
-                };
-
-                Some(s)
+                Some(symbol_from_typeargs(e.args()))
             }
-            ExternalOp::Opaque(_) => todo!(),
+            ExternalOp::Opaque(e) if e.name() == &SYM_OP_ID && e.extension() == &EXTENSION_ID => {
+                Some(symbol_from_typeargs(e.args()))
+            }
             _ => None,
         }
     } else {
