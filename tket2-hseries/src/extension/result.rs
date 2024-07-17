f826109acca7ef103ee90c36e872fc809cbef0d7
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2024-07-17 15:32:14 +0100

    feat: add results extensions
    Closes #452
    
    I'm not very pleased with this, but it does the job and all the "better" things I thought of didn't seem worth the extra code.
    
    Very open to suggestions.

---
+++ tket2-hseries/src/extension/result.rs
@@ -0,0 +1,564 @@
+//! This module defines the Hugr extension used to represent result reporting operations,
+//! with static string tags.
+//!
+use hugr::{
+    builder::{BuildError, Dataflow},
+    extension::{
+        prelude::{BOOL_T, PRELUDE, STRING_CUSTOM_TYPE},
+        simple_op::{try_from_name, MakeExtensionOp, MakeOpDef, MakeRegisteredOp, OpLoadError},
+        ExtensionId, ExtensionRegistry, OpDef, SignatureFunc,
+    },
+    ops::{CustomOp, NamedOp, OpType},
+    std_extensions::arithmetic::{
+        float_types::{EXTENSION as FLOAT_EXTENSION, FLOAT64_TYPE},
+        int_types::{EXTENSION as INT_EXTENSION, INT_TYPE_ID, LOG_WIDTH_TYPE_PARAM},
+    },
+    type_row,
+    types::{
+        type_param::{CustomTypeArg, TypeParam},
+        FunctionType, PolyFuncType, Type, TypeArg,
+    },
+    Extension, Wire,
+};
+
+use lazy_static::lazy_static;
+use serde::{Deserialize, Serialize};
+use strum_macros::{EnumIter, EnumString, IntoStaticStr};
+
+/// The "tket2.result" extension id.
+pub const EXTENSION_ID: ExtensionId = ExtensionId::new_unchecked("tket2.result");
+
+lazy_static! {
+    /// The "tket2.result" extension.
+    pub static ref EXTENSION: Extension = {
+        let mut ext = Extension::new(EXTENSION_ID);
+        BasicResultOp::load_all_ops(&mut ext).unwrap();
+        ArrayResultOp::load_all_ops(&mut ext).unwrap();
+        ext
+    };
+
+    /// Extension registry including the "tket2.result" extension and
+    /// dependencies.
+    pub static ref REGISTRY: ExtensionRegistry = ExtensionRegistry::try_new([
+        EXTENSION.to_owned(),
+        INT_EXTENSION.to_owned(),
+        FLOAT_EXTENSION.to_owned(),
+        PRELUDE.to_owned()
+    ]).unwrap();
+}
+
+#[derive(
+    Clone,
+    Copy,
+    Debug,
+    Serialize,
+    Deserialize,
+    Hash,
+    PartialEq,
+    Eq,
+    PartialOrd,
+    Ord,
+    EnumIter,
+    IntoStaticStr,
+    EnumString,
+)]
+#[allow(missing_docs)]
+#[non_exhaustive]
+/// Result report operations for simple types.
+/*
+result_int<Tag: StringArg, N: BoundedNat>( int<N> ) // N is bitwidth, e.g. i32, i64
+result_uint<Tag: StringArg, N: BoundedNat>( int<N> ) // unsigned
+result_bool<Tag: StringArg>( Sum((), ()) )
+result_f64<Tag: StringArg>( f64 )
+*/
+pub enum BasicResultOp {
+    #[strum(serialize = "result_bool")]
+    Bool,
+    #[strum(serialize = "result_int")]
+    Int,
+    #[strum(serialize = "result_uint")]
+    UInt,
+    #[strum(serialize = "result_f64")]
+    F64,
+}
+#[derive(
+    Clone,
+    Copy,
+    Debug,
+    Serialize,
+    Deserialize,
+    Hash,
+    PartialEq,
+    Eq,
+    PartialOrd,
+    Ord,
+    EnumIter,
+    IntoStaticStr,
+    EnumString,
+)]
+#[allow(missing_docs)]
+#[non_exhaustive]
+/// Result report operations for arrays of simple types.
+/*
+result_arr_int<Tag: StringArg, N: Nat, M: BoundedNat>( Array<N, int<M> > )
+result_arr_uint<Tag: StringArg, N: Nat, M: BoundedNat>( Array<N, int<M> > )
+result_arr_f64<Tag: StringArg, N: Nat>( Array<N,f64> )
+result_arr_bool<Tag: StringArg, N: Nat>( Array<N, Sum((), ()) > )*/
+pub enum ArrayResultOp {
+    #[strum(serialize = "result_array_bool")]
+    Bool,
+    #[strum(serialize = "result_array_int")]
+    Int,
+    #[strum(serialize = "result_array_uint")]
+    UInt,
+    #[strum(serialize = "result_array_f64")]
+    Float,
+}
+
+trait AsResultOp {
+    fn arg_type(&self) -> Type;
+    fn type_params(&self) -> Vec<TypeParam>;
+
+    fn result_signature(&self) -> SignatureFunc {
+        let string_param = TypeParam::Opaque {
+            ty: STRING_CUSTOM_TYPE,
+        };
+
+        PolyFuncType::new(
+            [vec![string_param], self.type_params()].concat(),
+            FunctionType::new(self.arg_type(), type_row![]),
+        )
+        .into()
+    }
+
+    fn instantiate(&self, args: &[TypeArg]) -> Result<ResultOp, OpLoadError>;
+}
+
+impl AsResultOp for BasicResultOp {
+    fn arg_type(&self) -> Type {
+        self.basic_op_type(1)
+    }
+
+    fn type_params(&self) -> Vec<TypeParam> {
+        match self {
+            Self::Bool | Self::F64 => vec![],
+            Self::Int | Self::UInt => vec![LOG_WIDTH_TYPE_PARAM],
+        }
+    }
+
+    fn instantiate(&self, args: &[TypeArg]) -> Result<ResultOp, OpLoadError> {
+        let args = concrete_result_op_type_args(args)?;
+        match args {
+            (tag, None, None) => Ok(ResultOp {
+                tag,
+                result_op: ResultOpDef::Basic(*self),
+                int_width: None,
+            }),
+            (tag, Some(width), None) => Ok(ResultOp {
+                tag,
+                result_op: ResultOpDef::Basic(*self),
+                int_width: Some(width as u8),
+            }),
+            _ => Err(hugr::extension::SignatureError::InvalidTypeArgs.into()),
+        }
+    }
+}
+
+impl BasicResultOp {
+    fn as_array_op(&self) -> ArrayResultOp {
+        match self {
+            Self::Bool => ArrayResultOp::Bool,
+            Self::Int => ArrayResultOp::Int,
+            Self::UInt => ArrayResultOp::UInt,
+            Self::F64 => ArrayResultOp::Float,
+        }
+    }
+
+    fn basic_op_type(&self, int_tv_idx: usize) -> Type {
+        match self {
+            Self::Bool => BOOL_T,
+            Self::Int | Self::UInt => INT_EXTENSION
+                .get_type(&INT_TYPE_ID)
+                .unwrap()
+                .instantiate(vec![TypeArg::new_var_use(int_tv_idx, LOG_WIDTH_TYPE_PARAM)])
+                .unwrap()
+                .into(),
+            Self::F64 => FLOAT64_TYPE,
+        }
+    }
+}
+impl ArrayResultOp {
+    fn inner_type_op(&self) -> BasicResultOp {
+        match self {
+            Self::Bool => BasicResultOp::Bool,
+            Self::Int => BasicResultOp::Int,
+            Self::UInt => BasicResultOp::UInt,
+            Self::Float => BasicResultOp::F64,
+        }
+    }
+}
+impl AsResultOp for ArrayResultOp {
+    fn arg_type(&self) -> Type {
+        let inner_t = self.inner_type_op().basic_op_type(2);
+        let size_var = TypeArg::new_var_use(1, TypeParam::max_nat());
+        PRELUDE
+            .get_type("array")
+            .unwrap()
+            .instantiate(vec![size_var, inner_t.into()])
+            .unwrap()
+            .into()
+    }
+
+    fn type_params(&self) -> Vec<TypeParam> {
+        [
+            vec![TypeParam::max_nat()],
+            self.inner_type_op().type_params(),
+        ]
+        .concat()
+    }
+
+    fn instantiate(&self, args: &[TypeArg]) -> Result<ResultOp, OpLoadError> {
+        let args = concrete_result_op_type_args(args)?;
+        match args {
+            (tag, Some(size), None) => Ok(ResultOp {
+                tag,
+                result_op: ResultOpDef::Array(*self, size),
+                int_width: None,
+            }),
+            (tag, Some(size), Some(width)) => Ok(ResultOp {
+                tag,
+                result_op: ResultOpDef::Array(*self, size),
+                int_width: Some(width as u8),
+            }),
+            _ => Err(hugr::extension::SignatureError::InvalidTypeArgs.into()),
+        }
+    }
+}
+
+impl MakeOpDef for BasicResultOp {
+    fn signature(&self) -> SignatureFunc {
+        self.result_signature()
+    }
+
+    fn from_def(op_def: &OpDef) -> Result<Self, hugr::extension::simple_op::OpLoadError> {
+        try_from_name(op_def.name(), &EXTENSION_ID)
+    }
+
+    fn extension(&self) -> ExtensionId {
+        EXTENSION_ID
+    }
+}
+
+impl MakeOpDef for ArrayResultOp {
+    fn signature(&self) -> SignatureFunc {
+        self.result_signature()
+    }
+
+    fn from_def(op_def: &OpDef) -> Result<Self, hugr::extension::simple_op::OpLoadError> {
+        try_from_name(op_def.name(), &EXTENSION_ID)
+    }
+
+    fn extension(&self) -> ExtensionId {
+        EXTENSION_ID
+    }
+}
+
+#[derive(Clone, Copy, Debug, Serialize, Deserialize, Hash, PartialEq, Eq)]
+enum ResultOpDef {
+    Basic(BasicResultOp),
+    Array(ArrayResultOp, u64),
+}
+
+#[derive(Clone, Debug, serde::Serialize, serde::Deserialize, Hash, PartialEq)]
+/// Concrete instantiation of a "tket2.result" operation.
+pub struct ResultOp {
+    tag: String,
+    result_op: ResultOpDef,
+    int_width: Option<u8>,
+}
+
+impl ResultOp {
+    fn _new_basic(tag: impl Into<String>, result_op: BasicResultOp) -> Self {
+        Self {
+            tag: tag.into(),
+            result_op: ResultOpDef::Basic(result_op),
+            int_width: None,
+        }
+    }
+
+    /// Create a new "tket2.result" operation for a boolean result.
+    pub fn new_bool(tag: impl Into<String>) -> Self {
+        Self::_new_basic(tag, BasicResultOp::Bool)
+    }
+
+    /// Create a new "tket2.result" operation for a floating-point result.
+    pub fn new_f64(tag: impl Into<String>) -> Self {
+        Self::_new_basic(tag, BasicResultOp::F64)
+    }
+
+    /// Convert this "tket2.result" operation to an array result operation over the same inner type.
+    /// The size of the array is set to the given value.
+    /// If this operation is already an array result operation, its size is updated.
+    pub fn array_op(mut self, size: u64) -> Self {
+        match &mut self.result_op {
+            ResultOpDef::Basic(op) => Self {
+                result_op: ResultOpDef::Array(op.as_array_op(), size),
+                ..self
+            },
+            ResultOpDef::Array(_, curr_size) => {
+                *curr_size = size;
+                self
+            }
+        }
+    }
+
+    /// Create a new "tket2.result" operation for a signed integer result of a given bit width.
+    pub fn new_int(tag: impl Into<String>, int_width: u8) -> Self {
+        Self {
+            tag: tag.into(),
+            result_op: ResultOpDef::Basic(BasicResultOp::Int),
+            int_width: Some(int_width),
+        }
+    }
+
+    /// Create a new "tket2.result" operation for an unsigned integer result of a given bit width.
+    pub fn new_uint(tag: impl Into<String>, int_width: u8) -> Self {
+        Self {
+            tag: tag.into(),
+            result_op: ResultOpDef::Basic(BasicResultOp::UInt),
+            int_width: Some(int_width),
+        }
+    }
+}
+
+fn concrete_result_op_type_args(
+    args: &[TypeArg],
+) -> Result<(String, Option<u64>, Option<u64>), OpLoadError> {
+    let err = || hugr::extension::SignatureError::InvalidTypeArgs.into();
+    let extract_string =
+        |arg: &CustomTypeArg| arg.value.as_str().map(|s| s.to_string()).ok_or(err());
+    match args {
+        [TypeArg::Opaque { arg }] => Ok((extract_string(arg)?, None, None)),
+
+        [TypeArg::Opaque { arg }, TypeArg::BoundedNat { n }] => {
+            Ok((extract_string(arg)?, Some(*n), None))
+        }
+
+        [TypeArg::Opaque { arg }, TypeArg::BoundedNat { n }, TypeArg::BoundedNat { n: m }] => {
+            Ok((extract_string(arg)?, Some(*n), Some(*m)))
+        }
+
+        _ => Err(err()),
+    }
+}
+
+impl<'a> From<&'a ResultOp> for &'static str {
+    fn from(value: &ResultOp) -> Self {
+        match &value.result_op {
+            ResultOpDef::Basic(op) => op.into(),
+            ResultOpDef::Array(op, _) => op.into(),
+        }
+    }
+}
+impl MakeExtensionOp for ResultOp {
+    fn from_extension_op(
+        ext_op: &hugr::ops::custom::ExtensionOp,
+    ) -> Result<Self, hugr::extension::simple_op::OpLoadError>
+    where
+        Self: Sized,
+    {
+        let def = ext_op.def();
+        let args = ext_op.args();
+        if let Ok(array_op) = ArrayResultOp::from_def(def) {
+            array_op.instantiate(args)
+        } else {
+            let basic_op = BasicResultOp::from_def(def)?;
+            basic_op.instantiate(args)
+        }
+    }
+
+    fn type_args(&self) -> Vec<TypeArg> {
+        let mut type_args = vec![TypeArg::Opaque {
+            arg: CustomTypeArg::new(STRING_CUSTOM_TYPE, self.tag.clone().into()).unwrap(),
+        }];
+
+        let basic_op = match self.result_op {
+            ResultOpDef::Basic(op) => op,
+            ResultOpDef::Array(op, size) => {
+                type_args.push(TypeArg::BoundedNat { n: size });
+                op.inner_type_op()
+            }
+        };
+
+        match basic_op {
+            BasicResultOp::Int | BasicResultOp::UInt => {
+                type_args.push(TypeArg::BoundedNat {
+                    n: self.int_width.expect("Int result op expects an int width.") as u64,
+                });
+            }
+            _ => {}
+        }
+
+        type_args
+    }
+}
+
+impl MakeRegisteredOp for ResultOp {
+    fn extension_id(&self) -> ExtensionId {
+        EXTENSION_ID
+    }
+
+    fn registry<'s, 'r: 's>(&'s self) -> &'r ExtensionRegistry {
+        &REGISTRY
+    }
+}
+
+impl TryFrom<&OpType> for BasicResultOp {
+    type Error = ();
+
+    fn try_from(value: &OpType) -> Result<Self, Self::Error> {
+        let Some(custom_op) = value.as_custom_op() else {
+            Err(())?
+        };
+        match custom_op {
+            CustomOp::Extension(ext) => Self::from_extension_op(ext).ok(),
+            CustomOp::Opaque(opaque) => try_from_name(opaque.name(), &EXTENSION_ID).ok(),
+        }
+        .ok_or(())
+    }
+}
+
+impl TryFrom<&OpType> for ArrayResultOp {
+    type Error = ();
+
+    fn try_from(value: &OpType) -> Result<Self, Self::Error> {
+        let Some(custom_op) = value.as_custom_op() else {
+            Err(())?
+        };
+        match custom_op {
+            CustomOp::Extension(ext) => Self::from_extension_op(ext).ok(),
+            CustomOp::Opaque(opaque) => try_from_name(opaque.name(), &EXTENSION_ID).ok(),
+        }
+        .ok_or(())
+    }
+}
+
+impl TryFrom<&OpType> for ResultOp {
+    type Error = OpLoadError;
+
+    fn try_from(value: &OpType) -> Result<Self, Self::Error> {
+        let Some(custom_op) = value.as_custom_op() else {
+            Err(OpLoadError::NotMember(value.name().into()))?
+        };
+        match custom_op {
+            CustomOp::Extension(ext) => Self::from_extension_op(ext),
+            CustomOp::Opaque(opaque) => {
+                if let Ok(basic_op) = try_from_name::<BasicResultOp>(opaque.name(), &EXTENSION_ID) {
+                    basic_op.instantiate(opaque.args())
+                } else if let Ok(array_op) =
+                    try_from_name::<ArrayResultOp>(opaque.name(), &EXTENSION_ID)
+                {
+                    array_op.instantiate(opaque.args())
+                } else {
+                    Err(OpLoadError::NotMember(opaque.name().to_string()))
+                }
+            }
+        }
+    }
+}
+
+/// An extension trait for [Dataflow] providing methods to add "tket2.result"
+/// operations.
+pub trait ResultOpBuilder: Dataflow {
+    /// Add a "tket2.result" op.
+    fn add_result(&mut self, result_wire: Wire, op: ResultOp) -> Result<(), BuildError> {
+        let handle = self.add_dataflow_op(op, [result_wire])?;
+
+        debug_assert_eq!(handle.outputs().len(), 0);
+        Ok(())
+    }
+}
+
+impl<D: Dataflow> ResultOpBuilder for D {}
+
+#[cfg(test)]
+pub(crate) mod test {
+    use cool_asserts::assert_matches;
+    use hugr::{
+        builder::{Dataflow, DataflowHugr, FunctionBuilder},
+        extension::prelude::array_type,
+        ops::NamedOp,
+        std_extensions::arithmetic::int_types::INT_TYPES,
+    };
+    use std::sync::Arc;
+    use strum::IntoEnumIterator;
+
+    use super::*;
+
+    fn get_opdef(op: impl NamedOp) -> Option<&'static Arc<OpDef>> {
+        EXTENSION.get_op(&op.name())
+    }
+
+    #[test]
+    fn create_extension() {
+        assert_eq!(EXTENSION.name(), &EXTENSION_ID);
+
+        for o in BasicResultOp::iter() {
+            assert_eq!(BasicResultOp::from_def(get_opdef(o).unwrap()), Ok(o));
+        }
+
+        for o in ArrayResultOp::iter() {
+            assert_eq!(ArrayResultOp::from_def(get_opdef(o).unwrap()), Ok(o));
+        }
+    }
+
+    #[test]
+    fn circuit() {
+        const ARR_SIZE: u64 = 20;
+        let in_row = vec![
+            BOOL_T,
+            FLOAT64_TYPE,
+            INT_TYPES[5].clone(),
+            INT_TYPES[6].clone(),
+        ];
+        let in_row = [
+            in_row.clone(),
+            in_row
+                .into_iter()
+                .map(|t| array_type(TypeArg::BoundedNat { n: ARR_SIZE }, t))
+                .collect(),
+        ]
+        .concat();
+        let hugr = {
+            let mut func_builder =
+                FunctionBuilder::new("circuit", FunctionType::new(in_row, type_row![])).unwrap();
+            let ops = [
+                ResultOp::new_bool("b"),
+                ResultOp::new_f64("f"),
+                ResultOp::new_int("i", 5),
+                ResultOp::new_uint("u", 6),
+            ];
+
+            for op in &ops {
+                let op_t: OpType = op.clone().to_extension_op().unwrap().into();
+                let new_op: ResultOp = (&op_t).try_into().unwrap();
+                assert_eq!(&new_op, op);
+            }
+            let [b, f, i, u, a_b, a_f, a_i, a_u] = func_builder.input_wires_arr();
+
+            for (w, op) in [b, f, i, u].iter().zip(ops.iter()) {
+                func_builder.add_result(*w, op.clone()).unwrap();
+            }
+            for (w, op) in [a_b, a_f, a_i, a_u].iter().zip(ops.iter()) {
+                func_builder
+                    .add_result(*w, op.clone().array_op(ARR_SIZE))
+                    .unwrap();
+            }
+
+            func_builder
+                .finish_hugr_with_outputs([], &REGISTRY)
+                .unwrap()
+        };
+        assert_matches!(hugr.validate(&REGISTRY), Ok(_));
+    }
+}
