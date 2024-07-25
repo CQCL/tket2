7b14f181b21b37eb89d8d4f1228606a7c0476e87
Agustín Borgna <121866228+aborgna-q@users.noreply.github.com>
2024-07-25 14:05:57 +0100

    chore: Replace build.rs with lint config in Cargo.toml (#509)
    Rust 1.80 [just
    dropped](https://blog.rust-lang.org/2024/07/25/Rust-1.80.0.html), adding
    a lint option that allows us to avoid the `build.rs` definition added in
    https://github.com/CQCL/tket2/pull/386.
    (The nightly lint got updated before hitting beta, so there shouldn't be
    compatibility problems there).

--- tket2/build.rs
+++
@@ -1,7 +0,0 @@
-//! Build script for the `tket2` crate.
-
-fn main() {
-    // We use a `ci_run` RUSTFLAG to indicate that we are running a CI check,
-    // so we can reject debug code using some tools defined in `utils.rs`.
-    println!("cargo:rustc-check-cfg=cfg(ci_run)");
-}
