f9cffc188f68453b0d4a1078e1ff56357ca83729
Agustin Borgna <agustin.borgna@quantinuum.com>
2024-07-29 11:08:11 +0100

    Apply review suggestions
    Co-authored-by: Douglas Wilson <douglas.wilson@quantinuum.com>

--- scripts/compile-test-eccs.sh
+++ scripts/compile-test-eccs.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # This script calls `compile-rewriter` to recompile all ECC sets in the
 # `test_files/eccs/` directory.
 #
 # Note that the output is not deterministic, so the resulting `.rwr` will always
 # be different even if the input and the compiler are the same.
+set -euo pipefail
 
 # Get the directory of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -16,7 +17,7 @@ COMPILE_REWRITER="cargo run --release -p compile-rewriter --"
 # Compile all json files into `.rwr`
 for ecc in $ECCS_DIR/*.json; do
     echo "-------- Compiling $ecc"
-    $COMPILE_REWRITER -i $ecc -o ${ecc%.json}.rwr
+    $COMPILE_REWRITER -i "$ecc" -o "${ecc%.json}.rwr"
 done
 
 # Additional hard-coded step:
@@ -27,6 +28,6 @@ nam_6_3="$ECCS_DIR/nam_6_3.rwr"
 PY_NAM_6_3="$PY_ECCS_DIR/nam_6_3.rwr"
 
 echo "Copying $nam_6_3 to $PY_NAM_6_3"
-cp -f $nam_6_3 $PY_NAM_6_3
+cp -f "$nam_6_3" "$PY_NAM_6_3"
 
 echo "Done!"
