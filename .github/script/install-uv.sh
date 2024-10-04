54cb878bf3028ae7b28c1c16ae7d36f67268d96f
Agustín Borgna <agustin.borgna@quantinuum.com>
2024-10-04 10:46:12 +0200

    ci: Use `setup-uv` action

--- .github/script/install-uv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Installs a fixed UV version
-#
-# This reads the `UV_CACHE_DIR` environment variable if it is set,
-# and stores the downloaded dependencies in that directory.
-
-curl -LsSf https://astral.sh/uv/0.3.4/install.sh | sh
