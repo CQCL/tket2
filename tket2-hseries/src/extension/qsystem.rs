501005d50d05c46b6a166f1508a86cdf97fd2003
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2024-11-26 16:03:57 +0000

    update extension name

--- tket2-hseries/src/extension/qsystem.rs
+++ tket2-hseries/src/extension/qsystem.rs
@@ -35,7 +35,7 @@ use lower::pi_mul_f64;
 pub use lower::{check_lowered, lower_tk2_op, LowerTk2Error, LowerTket2ToHSeriesPass};
 
 /// The "tket2.hseries" extension id.
-pub const EXTENSION_ID: ExtensionId = ExtensionId::new_unchecked("tket2.hseries");
+pub const EXTENSION_ID: ExtensionId = ExtensionId::new_unchecked("tket2.qsystem");
 /// The "tket2.hseries" extension version.
 pub const EXTENSION_VERSION: Version = Version::new(0, 2, 0);
 
