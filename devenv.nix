82dc8f624cdea872fcdcce8793143fdfc3fd1fd1
Seyon Sivarajah <seyon.sivarajah@cambridgequantum.com>
2025-03-17 10:19:15 +0000

    chore: devenv cleanups (#814)
    I started getting link errors on uv sync with mismatched symbols
    
    this repo no longer requires the llvm package, removing it fixed my
    problems

--- devenv.nix
+++ devenv.nix
@@ -1,16 +1,11 @@
-{ pkgs, lib, config, inputs, ... }:
-let
-  pkgs-2305 = import inputs.nixpkgs-2305 { system = pkgs.stdenv.system; };
-in
+{ pkgs, lib, ... }:
+
 {
   # https://devenv.sh/packages/
   # on macos frameworks have to be explicitly specified
   # otherwise a linker error ocurs on rust packages
   packages = [
     pkgs.just
-    pkgs.llvmPackages_16.libllvm
-    # cargo-llvm-cov is currently marked broken on nixpkgs unstable
-    pkgs-2305.cargo-llvm-cov
   ]
   ++ lib.optionals pkgs.stdenv.isLinux [
     pkgs.stdenv.cc.cc.lib
@@ -22,23 +17,11 @@ in
     ]
   );
 
-  # Certain Rust tools won't work without this
-  # This can also be fixed by using oxalica/rust-overlay and specifying the rust-src extension
-  # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela. for more details.
-  #env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = "echo Welcome to tket2proto devenv!";
 
   enterShell = ''
-    hello
     cargo --version
     python --version
     uv --version
-    export LLVM_COV="${pkgs.llvmPackages_16.libllvm}/bin/llvm-cov"
-    export LLVM_PROFDATA="${pkgs.llvmPackages_16.libllvm}/bin/llvm-profdata"
-
-    just setup
-    source .venv/bin/activate
   '';
 
   # https://devenv.sh/languages/
@@ -53,7 +36,10 @@ in
     enable = true;
     uv = {
       enable = true;
+      sync.enable = true;
     };
+    venv.enable = true;
   };
+
 
 }
