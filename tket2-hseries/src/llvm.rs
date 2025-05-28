12d2b7ac2885a73f7dc64023cf29b1a499446df7
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2025-05-28 18:40:18 +0100

    more renames

--- tket2-hseries/src/llvm.rs
+++ tket2-hseries/src/llvm.rs
@@ -1,8 +1,8 @@
 //! LLVM lowerings for HUGR extensions defined in this crate.
-pub mod array_struct;
+pub mod array_utils;
 pub mod futures;
 pub mod prelude;
 pub mod qsystem;
 pub mod random;
 pub mod result;
-pub mod utils_ext;
+pub mod utils;
