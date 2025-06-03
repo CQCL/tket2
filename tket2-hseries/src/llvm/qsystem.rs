0fafd6021c18c0480cda9cefd3a7cc14f0b9e960
Douglas Wilson <douglas.wilson@quantinuum.com>
2025-06-03 12:56:24 +0100

    refactor qsystem

--- tket2-hseries/src/llvm/qsystem.rs
+++ tket2-hseries/src/llvm/qsystem.rs
@@ -1,5 +1,8 @@
 //! LLVM lowering implementations for "tket2.qystem" operations.
 
+use hugr::llvm::extension::PreludeCodegen;
+use hugr::llvm::inkwell::types::FunctionType;
+use itertools::Itertools as _;
 use tket2::hugr::{self, llvm::inkwell};
 
 use crate::extension::qsystem::{self, QSystemOp};
@@ -8,9 +11,7 @@ use hugr::extension::prelude::qb_t;
 use hugr::llvm::custom::CodegenExtension;
 use hugr::llvm::emit::func::{build_option, EmitFuncContext};
 use hugr::llvm::emit::{emit_value, EmitOpArgs};
-use inkwell::builder::Builder;
-use inkwell::context::Context;
-use inkwell::types::{BasicType, FloatType, IntType};
+use inkwell::types::BasicType;
 use inkwell::values::{BasicValueEnum, FunctionValue, IntValue};
 use tket2::hugr::llvm::CodegenExtsBuilder;
 use tket2::hugr::ops::constant::Value;
@@ -20,9 +21,10 @@ use tket2::hugr::{HugrView, Node};
 use super::futures::future_type;
 
 /// Codegen extension for quantum operations.
-pub struct QSystemCodegenExtension;
+#[derive(derive_more::From)]
+pub struct QSystemCodegenExtension<PCG>(PCG);
 
-impl CodegenExtension for QSystemCodegenExtension {
+impl<PCG: PreludeCodegen> CodegenExtension for QSystemCodegenExtension<PCG> {
     fn add_extension<'a, H: HugrView<Node = Node> + 'a>(
         self,
         builder: CodegenExtsBuilder<'a, H>,
@@ -31,7 +33,7 @@ impl CodegenExtension for QSystemCodegenExtension {
         Self: 'a,
     {
         builder
-            .simple_extension_op(|context, args, op| QSystemEmitter(context).emit(args, op))
+            .simple_extension_op(move |context, args, op| self.emit(context, args, op))
             .extension_op(qsystem::EXTENSION_ID, qsystem::RUNTIME_BARRIER_NAME, {
                 move |context, args| {
                     // Do nothing
@@ -42,248 +44,271 @@ impl CodegenExtension for QSystemCodegenExtension {
     }
 }
 
-struct QSystemEmitter<'c, 'd, 'e, H: HugrView>(&'d mut EmitFuncContext<'c, 'e, H>);
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum RuntimeFunction {
+    Rz,
+    Rzz,
+    Rxy,
+    QAlloc,
+    QFree,
+    Measure,
+    LazyMeasure,
+    Reset,
+}
 
-impl<'c, H: HugrView<Node = Node>> QSystemEmitter<'c, '_, '_, H> {
-    fn iw_context(&self) -> &'c Context {
-        self.0.typing_session().iw_context()
+impl RuntimeFunction {
+    fn name(&self) -> &str {
+        match self {
+            RuntimeFunction::Rz => "___rz",
+            RuntimeFunction::Rzz => "___rzz",
+            RuntimeFunction::Rxy => "___rxy",
+            RuntimeFunction::QAlloc => "___qalloc",
+            RuntimeFunction::QFree => "___qfree",
+            RuntimeFunction::Measure => "___measure",
+            RuntimeFunction::LazyMeasure => "___lazy_measure",
+            RuntimeFunction::Reset => "___reset",
+        }
     }
 
-    fn int_type(&self) -> IntType<'c> {
-        self.iw_context().i64_type()
+    fn func_type<'c>(
+        &self,
+        context: &EmitFuncContext<'c, '_, impl HugrView<Node = Node>>,
+        pcg: &impl PreludeCodegen,
+    ) -> FunctionType<'c> {
+        let qb_type = pcg
+            .qubit_type(&context.typing_session())
+            .as_basic_type_enum();
+        let iwc = context.iw_context();
+        match self {
+            RuntimeFunction::Rz => iwc
+                .void_type()
+                .fn_type(&[qb_type.into(), iwc.f64_type().into()], false),
+            RuntimeFunction::Rzz => iwc.void_type().fn_type(
+                &[qb_type.into(), qb_type.into(), iwc.f64_type().into()],
+                false,
+            ),
+            RuntimeFunction::Rxy => iwc.void_type().fn_type(
+                &[qb_type.into(), iwc.f64_type().into(), iwc.f64_type().into()],
+                false,
+            ),
+            RuntimeFunction::QAlloc => qb_type.fn_type(&[], false),
+            RuntimeFunction::QFree => iwc.void_type().fn_type(&[qb_type.into()], false),
+            RuntimeFunction::Measure => iwc.bool_type().fn_type(&[qb_type.into()], false),
+            RuntimeFunction::LazyMeasure => future_type(iwc).fn_type(&[qb_type.into()], false),
+            RuntimeFunction::Reset => iwc.void_type().fn_type(&[qb_type.into()], false),
+        }
     }
 
-    fn qb_type(&self) -> IntType<'c> {
-        self.int_type()
+    fn get_func<'c, H: HugrView<Node = Node>>(
+        &self,
+        context: &EmitFuncContext<'c, '_, H>,
+        pcg: &impl PreludeCodegen,
+    ) -> Result<FunctionValue<'c>> {
+        let func_type = self.func_type(context, pcg);
+        context.get_extern_func(self.name(), func_type)
     }
+}
 
-    fn float_type(&self) -> FloatType<'c> {
-        self.iw_context().f64_type()
-    }
-
-    fn bool_type(&self) -> IntType<'c> {
-        self.iw_context().bool_type()
-    }
-
-    fn builder(&self) -> &Builder<'c> {
-        self.0.builder()
-    }
-
-    fn get_func_rz(&self) -> Result<FunctionValue<'c>> {
-        let func_type = self
-            .iw_context()
-            .void_type()
-            .fn_type(&[self.qb_type().into(), self.float_type().into()], false);
-        self.0.get_extern_func("___rz", func_type)
-    }
-
-    fn get_func_rzz(&self) -> Result<FunctionValue<'c>> {
-        let func_type = self.iw_context().void_type().fn_type(
-            &[
-                self.qb_type().into(),
-                self.qb_type().into(),
-                self.float_type().into(),
-            ],
-            false,
-        );
-        self.0.get_extern_func("___rzz", func_type)
-    }
-
-    fn get_func_rxy(&self) -> Result<FunctionValue<'c>> {
-        let func_type = self.iw_context().void_type().fn_type(
-            &[
-                self.qb_type().into(),
-                self.float_type().into(),
-                self.float_type().into(),
-            ],
-            false,
-        );
-        self.0.get_extern_func("___rxy", func_type)
-    }
-
-    fn get_func_qalloc(&self) -> Result<FunctionValue<'c>> {
-        let func_type = self.qb_type().fn_type(&[], false);
-        self.0.get_extern_func("___qalloc", func_type)
-    }
-
-    fn get_func_qfree(&self) -> Result<FunctionValue<'c>> {
-        let func_type = self
-            .iw_context()
-            .void_type()
-            .fn_type(&[self.qb_type().into()], false);
-        self.0.get_extern_func("___qfree", func_type)
-    }
-
-    fn get_func_measure(&self) -> Result<FunctionValue<'c>> {
-        let func_type = self.bool_type().fn_type(&[self.qb_type().into()], false);
-        self.0.get_extern_func("___measure", func_type)
-    }
-
-    fn get_func_lazy_measure(&self) -> Result<FunctionValue<'c>> {
-        let func_type = future_type(self.iw_context()).fn_type(&[self.qb_type().into()], false);
-        self.0.get_extern_func("___lazy_measure", func_type)
-    }
-
-    fn get_func_reset(&self) -> Result<FunctionValue<'c>> {
-        let func_type = self
-            .iw_context()
-            .void_type()
-            .fn_type(&[self.qb_type().into()], false);
-        self.0.get_extern_func("___reset", func_type)
+impl<PCG: PreludeCodegen> QSystemCodegenExtension<PCG> {
+    fn runtime_func<'c>(
+        &self,
+        context: &EmitFuncContext<'c, '_, impl HugrView<Node = Node>>,
+        rf: RuntimeFunction,
+    ) -> Result<FunctionValue<'c>> {
+        rf.get_func(context, &self.0)
     }
 
     /// Helper function to `emit` a qsystem operation.
-    fn emit_op(
+    fn emit_impl<'c, H: HugrView<Node = Node>>(
         &self,
+        context: &mut EmitFuncContext<'c, '_, impl HugrView<Node = Node>>,
         args: EmitOpArgs<'c, '_, ExtensionOp, H>,
-        name: &str,
-        func: Result<FunctionValue<'c>>,
+        runtime_func: RuntimeFunction,
         input_indices: &[usize],
         output_indices: &[usize],
     ) -> Result<()> {
-        let inputs: Vec<_> = input_indices.iter().map(|&i| args.inputs[i]).collect();
-        let outputs: Vec<_> = output_indices.iter().map(|&i| args.inputs[i]).collect();
-        self.builder().build_call(
-            func?,
-            &inputs.iter().map(|&v| v.into()).collect::<Vec<_>>(),
-            name,
-        )?;
-        args.outputs.finish(self.builder(), outputs)
+        let inputs = input_indices
+            .iter()
+            .map(|&i| args.inputs[i].into())
+            .collect_vec();
+        let outputs = output_indices.iter().map(|&i| args.inputs[i]).collect_vec();
+        let func = self.runtime_func(context, runtime_func)?;
+        context
+            .builder()
+            .build_call(func, &inputs, runtime_func.name())?;
+        args.outputs.finish(context.builder(), outputs)
     }
 
     /// Function to help lower the `tket2.qsystem` extension.
-    fn emit(&mut self, args: EmitOpArgs<'c, '_, ExtensionOp, H>, op: QSystemOp) -> Result<()> {
+    fn emit<'c, H: HugrView<Node = Node>>(
+        &self,
+        context: &mut EmitFuncContext<'c, '_, H>,
+        args: EmitOpArgs<'c, '_, ExtensionOp, H>,
+        op: QSystemOp,
+    ) -> Result<()> {
         match op {
             // Rotation about Z
-            QSystemOp::Rz => self.emit_op(args, "rz", self.get_func_rz(), &[0, 1], &[0]),
+            QSystemOp::Rz => self.emit_impl(context, args, RuntimeFunction::Rz, &[0, 1], &[0]),
             // Rotation about ZZ (aka "ZZPhase")
             QSystemOp::ZZPhase => {
-                self.emit_op(args, "rzz", self.get_func_rzz(), &[0, 1, 2], &[0, 1])
+                self.emit_impl(context, args, RuntimeFunction::Rzz, &[0, 1, 2], &[0, 1])
             }
             // Rotation in X and Y
             // (α,β) ↦ Rz(β)Rx(α)Rz(−β)
             //
             // aka "PhasedX", "R1XY", "U1q"
-            QSystemOp::PhasedX => self.emit_op(args, "rxy", self.get_func_rxy(), &[0, 1, 2], &[0]),
+            QSystemOp::PhasedX => {
+                self.emit_impl(context, args, RuntimeFunction::Rxy, &[0, 1, 2], &[0])
+            }
             // Measure qubit in Z basis
             QSystemOp::Measure | QSystemOp::MeasureReset => {
+                let true_val = emit_value(context, &Value::true_val())?;
+                let false_val = emit_value(context, &Value::false_val())?;
+                let builder = context.builder();
                 let [qb] = args
                     .inputs
                     .try_into()
                     .map_err(|_| anyhow!("Measure expects one input"))?;
-                let measure = self.get_func_measure()?;
-                let result_i1 = self
-                    .builder()
-                    .build_call(measure, &[qb.into()], "measure_i1")?
+                let result_i1 = builder
+                    .build_call(
+                        self.runtime_func(context, RuntimeFunction::Measure)?,
+                        &[qb.into()],
+                        "measure_i1",
+                    )?
                     .try_as_basic_value()
                     .unwrap_left()
                     .into_int_value();
-                let true_val = emit_value(self.0, &Value::true_val())?;
-                let false_val = emit_value(self.0, &Value::false_val())?;
-                let result = self
-                    .builder()
-                    .build_select(result_i1, true_val, false_val, "measure")?;
+                let result = builder.build_select(result_i1, true_val, false_val, "measure")?;
                 if op == QSystemOp::Measure {
                     // normal measure may put the qubit in invalid state, so assume
                     // deallocation, don't return it
-                    self.builder()
-                        .build_call(self.get_func_qfree()?, &[qb.into()], "qfree")?;
-                    args.outputs.finish(self.builder(), [result])
+                    builder.build_call(
+                        self.runtime_func(context, RuntimeFunction::QFree)?,
+                        &[qb.into()],
+                        "qfree",
+                    )?;
+                    args.outputs.finish(builder, [result])
                 } else {
                     // MeasureReset will reset the qubit after measurement so safe to return
-                    self.builder()
-                        .build_call(self.get_func_reset()?, &[qb.into()], "reset")?;
-                    args.outputs.finish(self.builder(), [qb, result])
+                    builder.build_call(
+                        self.runtime_func(context, RuntimeFunction::Reset)?,
+                        &[qb.into()],
+                        "reset",
+                    )?;
+                    args.outputs.finish(builder, [qb, result])
                 }
             }
             // Measure qubit in Z basis, not forcing to a boolean
             QSystemOp::LazyMeasure => {
+                let builder = context.builder();
                 let [qb] = args
                     .inputs
                     .try_into()
                     .map_err(|_| anyhow!("LazyMeasure expects one input"))?;
-                let result = self
-                    .builder()
-                    .build_call(self.get_func_lazy_measure()?, &[qb.into()], "lazy_measure")?
+                let result = builder
+                    .build_call(
+                        self.runtime_func(context, RuntimeFunction::LazyMeasure)?,
+                        &[qb.into()],
+                        "lazy_measure",
+                    )?
                     .try_as_basic_value()
                     .unwrap_left();
-                let qfree = self.get_func_qfree()?;
-                self.builder().build_call(qfree, &[qb.into()], "qfree")?;
-                args.outputs.finish(self.builder(), [result])
+                builder.build_call(
+                    self.runtime_func(context, RuntimeFunction::QFree)?,
+                    &[qb.into()],
+                    "qfree",
+                )?;
+                args.outputs.finish(builder, [result])
             }
             QSystemOp::LazyMeasureReset => {
+                let builder = context.builder();
                 let [qb] = args
                     .inputs
                     .try_into()
                     .map_err(|_| anyhow!("LazyMeasureReset expects one input"))?;
-                let measure = self.get_func_lazy_measure()?;
-                let result = self
-                    .builder()
-                    .build_call(measure, &[qb.into()], "lazy_measure")?
+                let result = builder
+                    .build_call(
+                        self.runtime_func(context, RuntimeFunction::LazyMeasure)?,
+                        &[qb.into()],
+                        "lazy_measure",
+                    )?
                     .try_as_basic_value()
                     .unwrap_left();
-                let reset = self.get_func_reset()?;
-                self.builder().build_call(reset, &[qb.into()], "reset")?;
-                args.outputs.finish(self.builder(), [qb, result])
+                builder.build_call(
+                    self.runtime_func(context, RuntimeFunction::Reset)?,
+                    &[qb.into()],
+                    "reset",
+                )?;
+                args.outputs.finish(builder, [qb, result])
             }
             // Reset a qubit
-            QSystemOp::Reset => self.emit_op(args, "reset", self.get_func_reset(), &[0], &[0]),
+            QSystemOp::Reset => self.emit_impl(context, args, RuntimeFunction::Reset, &[0], &[0]),
             QSystemOp::TryQAlloc => {
                 let [] = args
                     .inputs
                     .try_into()
                     .map_err(|_| anyhow!("QAlloc expects no inputs"))?;
-                let qb = self
+                let qb = context
                     .builder()
-                    .build_call(self.get_func_qalloc()?, &[], "qalloc")?
+                    .build_call(
+                        self.runtime_func(context, RuntimeFunction::QAlloc)?,
+                        &[],
+                        "qalloc",
+                    )?
                     .try_as_basic_value()
                     .unwrap_left();
 
-                let max_qb = self.qb_type().const_int(u64::MAX, false);
-                let not_max = self.builder().build_int_compare(
+                let max_qb = self
+                    .0
+                    .qubit_type(&context.typing_session())
+                    .as_basic_type_enum()
+                    .into_int_type()
+                    .const_int(u64::MAX, false);
+                let not_max = context.builder().build_int_compare(
                     inkwell::IntPredicate::NE,
                     qb.into_int_value(),
                     max_qb,
                     "not_max",
                 )?;
-                self.reset_if_qb(qb, not_max)?;
+                self.reset_if_qb(context, qb, not_max)?;
 
-                let result = build_option(self.0, not_max, qb, qb_t())?;
-                args.outputs.finish(self.builder(), [result])
+                let result = build_option(context, not_max, qb, qb_t())?;
+                args.outputs.finish(context.builder(), [result])
             }
-            QSystemOp::QFree => self.emit_op(args, "qfree", self.get_func_qfree(), &[0], &[]),
+            QSystemOp::QFree => self.emit_impl(context, args, RuntimeFunction::QFree, &[0], &[]),
         }
     }
 
     /// Reset a qubit if it is successfully allocated (not max value)
-    fn reset_if_qb(
+    fn reset_if_qb<'c>(
         &self,
+        context: &mut EmitFuncContext<'c, '_, impl HugrView<Node = Node>>,
         qb: BasicValueEnum<'c>,
-        not_max: IntValue<'_>,
-    ) -> Result<(), anyhow::Error> {
-        let orig_bb = self
+        not_max: IntValue<'c>,
+    ) -> Result<()> {
+        let orig_bb = context
             .builder()
             .get_insert_block()
             .ok_or_else(|| anyhow!("No current insertion point"))?;
 
-        let reset_bb = self
+        let id_bb = context
             .iw_context()
-            .insert_basic_block_after(orig_bb, "reset_bb");
-        let id_bb = self
-            .iw_context()
-            .insert_basic_block_after(reset_bb, "id_bb");
-        let reset_bb = {
-            self.builder().position_at_end(reset_bb);
-            let reset = self.get_func_reset()?;
-            self.builder().build_call(reset, &[qb.into()], "reset")?;
-            self.builder().build_unconditional_branch(id_bb)?;
-            reset_bb
-        };
+            .insert_basic_block_after(orig_bb, "id_bb");
 
-        self.builder().position_at_end(orig_bb);
-        self.builder()
+        let reset_bb =
+            context.build_positioned_new_block("reset_bb", Some(id_bb), |context, bb| {
+                context.builder().build_call(
+                    self.runtime_func(context, RuntimeFunction::Reset)?,
+                    &[qb.into()],
+                    "reset",
+                )?;
+                context.builder().build_unconditional_branch(id_bb)?;
+                anyhow::Ok(bb)
+            })?;
+        context
+            .builder()
             .build_conditional_branch(not_max, reset_bb, id_bb)?;
-        self.builder().position_at_end(id_bb);
+        context.builder().position_at_end(id_bb);
         Ok(())
     }
 }
@@ -321,7 +346,7 @@ mod test {
         use crate::llvm::{futures::FuturesCodegenExtension, prelude::QISPreludeCodegen};
 
         llvm_ctx.add_extensions(|ceb| {
-            ceb.add_extension(QSystemCodegenExtension)
+            ceb.add_extension(QSystemCodegenExtension(QISPreludeCodegen))
                 .add_extension(FuturesCodegenExtension)
                 .add_prelude_extensions(QISPreludeCodegen)
                 .add_float_extensions()
