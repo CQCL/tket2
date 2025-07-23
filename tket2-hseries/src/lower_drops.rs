18104dca5ec3a6393094fd25e5ac382e2455334e
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2025-07-23 12:05:08 +0100

    Test, add copy_discard_array handler

--- tket2-hseries/src/lower_drops.rs
+++ tket2-hseries/src/lower_drops.rs
@@ -1,4 +1,6 @@
-use hugr::algorithms::replace_types::{NodeTemplate, ReplaceTypesError, ReplacementOptions};
+use hugr::algorithms::replace_types::{
+    handlers::copy_discard_array, NodeTemplate, ReplaceTypesError, ReplacementOptions,
+};
 use hugr::algorithms::{ComposablePass, ReplaceTypes};
 use hugr::builder::{Container, DFGBuilder};
 use hugr::types::{Signature, Term};
@@ -16,6 +18,8 @@ impl<H: HugrMut<Node = Node>> ComposablePass<H> for LowerDropsPass {
 
     fn run(&self, hugr: &mut H) -> Result<Self::Result, Self::Error> {
         let mut rt = ReplaceTypes::default();
+        rt.linearizer()
+            .register_callback(array_type_def(), copy_discard_array);
         rt.replace_parametrized_op_with(
             GUPPY_EXTENSION.get_op(DROP_OP_NAME.as_str()).unwrap(),
             |targs| {
@@ -30,5 +34,41 @@ impl<H: HugrMut<Node = Node>> ComposablePass<H> for LowerDropsPass {
             ReplacementOptions::default().with_linearization(true),
         );
         rt.run(hugr)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::sync::Arc;
+
+    use hugr::builder::{inout_sig, Dataflow, DataflowHugr};
+    use hugr::ops::ExtensionOp;
+    use hugr::{extension::prelude::usize_t, std_extensions::collections::array::array_type};
+    use hugr::{Hugr, HugrView};
+
+    use super::*;
+
+    #[test]
+    fn test_lower_drop() {
+        let arr_type = array_type(2, usize_t());
+        let drop_op = GUPPY_EXTENSION.get_op(DROP_OP_NAME.as_str()).unwrap();
+        let drop_node = ExtensionOp::new(drop_op.clone(), [arr_type.clone().into()]).unwrap();
+        let mut b = DFGBuilder::new(inout_sig(arr_type, vec![])).unwrap();
+        let inp = b.input_wires();
+        b.add_dataflow_op(drop_node, inp).unwrap();
+        let mut h = b.finish_hugr_with_outputs([]).unwrap();
+        let count_drops = |h: &Hugr| {
+            h.nodes()
+                .filter(|n| {
+                    h.get_optype(*n)
+                        .as_extension_op()
+                        .is_some_and(|e| Arc::ptr_eq(e.def_arc(), drop_op))
+                })
+                .count()
+        };
+        assert_eq!(count_drops(&h), 1);
+        LowerDropsPass.run(&mut h).unwrap();
+        h.validate().unwrap();
+        assert_eq!(count_drops(&h), 0);
     }
 }
