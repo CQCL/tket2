e9be6df9007554c6cc3b507db9cb3f0c3909e250
Craig Roy <craig.roy@quantinuum.com>
2025-07-24 11:24:09 +0100

    Add test that extensions validate
    This shows that tket2-exts is currently broken!

---
+++ tket2-exts/tests/test_validate_exts.py
@@ -0,0 +1,36 @@
+from tket2_exts import (
+    opaque_bool,
+    debug,
+    guppy,
+    rotation,
+    futures,
+    qsystem,
+    qsystem_random,
+    qsystem_utils,
+    quantum,
+    result,
+    wasm,
+)
+
+import pytest
+
+
+exts = [
+    opaque_bool,
+    debug,
+    guppy,
+    rotation,
+    futures,
+    qsystem,
+    qsystem_random,
+    qsystem_utils,
+    quantum,
+    result,
+    wasm,
+]
+
+
+@pytest.mark.parametrize("ext", exts)
+def test_validate_extension(ext):
+    ext()
+    return
