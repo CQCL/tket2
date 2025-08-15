ae40c629fe274bae9e6e125907a0066197248352
Agustín Borgna <agustin.borgna@quantinuum.com>
2025-08-15 15:34:57 +0100

    Add TODO

--- tket/src/serialize/pytket/decoder/tracked_elem.rs
+++ tket/src/serialize/pytket/decoder/tracked_elem.rs
@@ -62,6 +62,9 @@ pub struct TrackedBit {
     reg: Arc<PytketRegister>,
     /// The hash of the pytket register for this tracked element, used to
     /// speed up hashing and equality checks.
+    //
+    // TODO: We could put this along with `reg` in a `PytketResource` struct
+    // that gets used around the crate.
     reg_hash: RegisterHash,
 }
 
