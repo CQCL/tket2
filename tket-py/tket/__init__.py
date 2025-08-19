1e737896619d612c866c5afef2b12cda2e91cce2
hugrbot <CI_testing@quantinuum.com>
2025-08-19 17:08:24 +0100

    chore(py): release tket-py 0.12.2 (#1006)
    :robot: I have created a release *beep* *boop*
    ---
    
    
    ##
    [0.12.2](https://github.com/CQCL/tket2/compare/tket-py-v0.12.1...tket-py-v0.12.2)
    (2025-08-19)
    
    
    ### Features
    
    * Define a wire tracker for the new pytket decoder
    ([#1036](https://github.com/CQCL/tket2/issues/1036))
    ([2466ee2](https://github.com/CQCL/tket2/commit/2466ee26ab75b4e62136bd151d55f25ce8d1adbd))
    * Support qsystem native operations when loading pytket circuits
    ([#1041](https://github.com/CQCL/tket2/issues/1041))
    ([88c5c79](https://github.com/CQCL/tket2/commit/88c5c7920fe954d59d8dc8460939ba0f29f306d4))
    * **tket-py:** Create BadgerOptimiser from arbitrary Rewriters
    ([#1022](https://github.com/CQCL/tket2/issues/1022))
    ([a975c1d](https://github.com/CQCL/tket2/commit/a975c1db0ca1f586cd0e64bbd6054f8aa6ed62b9)),
    closes [#1021](https://github.com/CQCL/tket2/issues/1021)
    
    
    ### Documentation
    
    * Update README badges
    ([#1004](https://github.com/CQCL/tket2/issues/1004))
    ([d609bf5](https://github.com/CQCL/tket2/commit/d609bf5f65af3cfe3ac44a16dfd4ef1bcacd5643))
    
    ---
    This PR was generated with [Release
    Please](https://github.com/googleapis/release-please). See
    [documentation](https://github.com/googleapis/release-please#release-please).
    
    ---------
    
    Co-authored-by: Agustín Borgna <agustin.borgna@quantinuum.com>

--- tket-py/tket/__init__.py
+++ tket-py/tket/__init__.py
@@ -16,4 +16,4 @@ __all__ = ["circuit", "ops", "optimiser", "passes", "pattern", "rewrite"]
 
 # This is updated by our release-please workflow, triggered by this
 # annotation: x-release-please-version
-__version__ = "0.12.1"
+__version__ = "0.12.2"
