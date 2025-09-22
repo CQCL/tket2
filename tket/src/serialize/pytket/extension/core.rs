58f7698bf15caeaddebc572722a53b15adf1b67e
Agustín Borgna <agustin.borgna@quantinuum.com>
2025-09-22 11:13:08 +0100

    feat!(pytket-decoder): Allow specifying qubit/bit reuse

--- tket/src/serialize/pytket/extension/core.rs
+++ tket/src/serialize/pytket/extension/core.rs
@@ -40,10 +40,8 @@ impl PytketDecoder for CoreDecoder {
         match (op.op_type, &op.op_box) {
             (PytketOptype::CircBox, Some(OpBox::CircBox { id: _id, circuit })) => {
                 // We have no way to distinguish between input and output bits in the circuit box, so we assume all bits are outputs here.
-                //
-                // TODO: Pass the registers both as inputs and outputs once this is implemented
-                // <https://github.com/CQCL/tket2/issues/1124>
                 let circ_inputs: Vec<Type> = itertools::repeat_n(qb_t(), qubits.len())
+                    .chain(itertools::repeat_n(bool_t(), bits.len()))
                     .chain(itertools::repeat_n(rotation_type(), params.len()))
                     .collect_vec();
                 let circ_outputs: Vec<Type> = itertools::repeat_n(qb_t(), qubits.len())
@@ -62,7 +60,7 @@ impl PytketDecoder for CoreDecoder {
                     circuit.decode_inplace(decoder.builder.hugr_mut(), target, options)?;
 
                 decoder
-                    .wire_up_node(internal, qubits, bits, params)
+                    .wire_up_node(internal, qubits, qubits, bits, bits, params)
                     .map_err(|e| e.hugr_op("DFG"))?;
 
                 Ok(DecodeStatus::Success)
