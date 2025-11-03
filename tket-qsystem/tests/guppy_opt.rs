0cac5fcf2be274dd2d0f0e1411c3b24eb014232e
Agustín Borgna <121866228+aborgna-q@users.noreply.github.com>
2025-11-03 18:36:40 +0000

    feat: `NormalizeGuppy` pass to simplify generated structure (#1220)
    A pass that simplifies the structure of guppy-generated hugrs so we can
    optimise them more effectively

--- tket-qsystem/tests/guppy_opt.rs
+++ tket-qsystem/tests/guppy_opt.rs
@@ -1,22 +1,13 @@
 //! Tests optimizing Guppy-generated programs.
 
-use std::convert::Infallible;
 use std::fs;
 use std::io::BufReader;
 use std::path::Path;
 
-use hugr::algorithms::const_fold::ConstFoldError;
-use hugr::algorithms::inline_dfgs::InlineDFGsPass;
-use hugr::algorithms::normalize_cfgs::merge_basic_blocks;
-use hugr::algorithms::untuple::{UntupleError, UntupleRecursive};
 use hugr::algorithms::ComposablePass;
-use hugr::algorithms::UntuplePass;
-use hugr::algorithms::{RemoveDeadFuncsError, RemoveDeadFuncsPass};
-use hugr::hugr::hugrmut::HugrMut;
-use hugr::hugr::patch::inline_dfg::InlineDFGError;
 use hugr::{Hugr, HugrView};
-use itertools::Itertools;
 use rstest::{fixture, rstest};
+use tket::passes::NormalizeGuppy;
 use tket_qsystem::QSystemPass;
 
 const GUPPY_EXAMPLES_DIR: &str = "../test_files/guppy_optimization";
@@ -65,42 +56,7 @@ fn guppy_simple_cx() -> Hugr {
 #[case::simple_cx(guppy_simple_cx())]
 #[cfg_attr(miri, ignore)] // Opening files is not supported in (isolated) miri
 fn optimise_guppy(#[case] mut hugr: Hugr) {
-    // Merge basic blocks with trivially deterministic branching.
-    //
-    // This should be a composable pass in hugr-passes.
-    // See <https://github.com/CQCL/hugr/issues/2556>
-    let cfgs = hugr
-        .entry_descendants()
-        .filter(|&n| hugr.get_optype(n).is_cfg())
-        .collect_vec();
-    for cfg in cfgs {
-        let mut rerooted = hugr.with_entrypoint_mut(cfg);
-        merge_basic_blocks(&mut rerooted).unwrap();
-    }
-
-    let untuple = UntuplePass::new(UntupleRecursive::Recursive);
-    // TODO: Constant folding fails on some test cases (it leaves qubit ports disconnected).
-    // See <https://github.com/CQCL/hugr/issues/2557>
-    let dead_funcs = RemoveDeadFuncsPass::default();
-    let inline = InlineDFGsPass;
-    // TODO: We are missing a pass to inline straight-line CFGs here.
-
-    /// Helper error accumulator
-    #[derive(derive_more::Error, Debug, derive_more::Display, derive_more::From)]
-    enum OptErrors {
-        ConstantFold(ConstFoldError),
-        Untuple(UntupleError),
-        DeadFuncs(RemoveDeadFuncsError),
-        Dce(Infallible),
-        Inline(InlineDFGError),
-    }
-
-    untuple
-        .map_err(OptErrors::Untuple)
-        .then::<_, OptErrors>(dead_funcs)
-        .then::<_, OptErrors>(inline)
-        .run(&mut hugr)
-        .unwrap();
+    NormalizeGuppy::default().run(&mut hugr).unwrap();
 
     // TODO: Run pytket passes here, and check that the circuit is as optimized as possible at this point.
     //
