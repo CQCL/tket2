fb6efb205d72545937af2c4a016a09c153a04190
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2025-11-05 13:46:26 +0000

    generate hugrs for simple test cases

--- test_files/guppy_optimization/1q_squash/qsystem_chain.opt.py
+++ test_files/guppy_optimization/1q_squash/qsystem_chain.opt.py
@@ -1,10 +1,17 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "guppylang >=0.21.3",
+# ]
+# ///
+
 from pathlib import Path
 from sys import argv
 
 from guppylang import guppy
-from guppy.std.quantum import qubit
+from guppylang.std.quantum import qubit
 from guppylang.std.qsystem import rz, phased_x
-from guppylang.std.builtins import angle
+from guppylang.std.angles import angle
 
 
 @guppy
@@ -13,5 +20,5 @@ def qsystem_chain(q: qubit) -> None:
     rz(q, angle(0.870616))
 
 
-program = qsystem_chain.compile()
+program = qsystem_chain.compile_function()
 Path(argv[0]).with_suffix(".hugr").write_bytes(program.to_bytes())
--- test_files/guppy_optimization/1q_squash/rz_chain.py
+++ test_files/guppy_optimization/1q_squash/rz_chain.py
@@ -1,9 +1,16 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "guppylang >=0.21.3",
+# ]
+# ///
+
 from pathlib import Path
 from sys import argv
 
 from guppylang import guppy
 from guppylang.std.quantum import rz, qubit
-from guppylang.std.builtins import angle
+from guppylang.std.angles import angle
 
 
 @guppy
@@ -13,5 +20,5 @@ def rz_chain(q: qubit) -> None:
     rz(q, angle(1 / 2))
 
 
-program = rz_chain.compile()
+program = rz_chain.compile_function()
 Path(argv[0]).with_suffix(".hugr").write_bytes(program.to_bytes())
