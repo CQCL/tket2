d396ff2a01e1f1eb8d9f47ea4bad4e9317ade3ef
Agustín Borgna <agustin.borgna@quantinuum.com>
2025-11-07 16:46:01 +0000

    ???

--- tket1-passes/cmake-toolchain-mingw.cmake
+++ tket1-passes/cmake-toolchain-mingw.cmake
@@ -1,21 +1,32 @@
 # CMake toolchain file for mingw-w64 cross-compilation from Linux
 # This file is used in CI builds to ensure CMake uses GCC-style flags instead of MSVC flags
+# Note: This file is included AFTER Conan's toolchain file, so Conan's CMAKE_PREFIX_PATH is already set
+
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
 # Specify the cross compiler
-set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
-set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc CACHE FILEPATH "C compiler" FORCE)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++ CACHE FILEPATH "C++ compiler" FORCE)
 
 # Tell CMake to use GCC-style flags, not MSVC
-set(CMAKE_C_COMPILER_ID "GNU")
-set(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_C_COMPILER_ID "GNU" CACHE STRING "C compiler ID" FORCE)
+set(CMAKE_CXX_COMPILER_ID "GNU" CACHE STRING "C++ compiler ID" FORCE)
 
-# Disable MSVC-specific flags
-set(CMAKE_C_FLAGS_INIT "")
-set(CMAKE_CXX_FLAGS_INIT "")
-set(CMAKE_C_FLAGS_RELEASE_INIT "")
-set(CMAKE_CXX_FLAGS_RELEASE_INIT "")
+# Ensure package discovery works by preserving CMAKE_PREFIX_PATH and setting find modes
+# Conan sets CMAKE_PREFIX_PATH, so we need to ensure packages can be found
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
 
-# Use Unix Makefiles generator
-set(CMAKE_GENERATOR "Unix Makefiles" CACHE STRING "" FORCE)
+# Clear MSVC-specific flags that might be set by Conan's toolchain
+# We do this after Conan's toolchain has run, so we can filter out MSVC flags
+if(CMAKE_CXX_FLAGS MATCHES "/WX|/EHsc")
+  string(REGEX REPLACE "/WX[^ ]*" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+  string(REGEX REPLACE "/EHsc[^ ]*" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}" CACHE STRING "C++ flags" FORCE)
+endif()
+if(CMAKE_C_FLAGS MATCHES "/WX")
+  string(REGEX REPLACE "/WX[^ ]*" "" CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "C flags" FORCE)
+endif()
