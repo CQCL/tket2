8974ab52f81f67b2ed2a4b4e2f8ab84c904fdb3a
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2025-11-12 13:06:42 +0000

    count_gates returns map

--- tket-qsystem/tests/guppy_opt.rs
+++ tket-qsystem/tests/guppy_opt.rs
@@ -1,5 +1,7 @@
 //! Tests optimizing Guppy-generated programs.
 
+use smol_str::SmolStr;
+use std::collections::HashMap;
 use std::fs;
 use std::io::BufReader;
 use std::path::Path;
@@ -45,23 +47,16 @@ fn guppy_simple_cx() -> Hugr {
     load_guppy_circuit("simple_cx")
 }
 
-fn count_gates(h: &impl HugrView) -> (usize, usize) {
-    let mut non_alloc = 0;
-    let mut alloc = 0;
+fn count_gates(h: &impl HugrView) -> HashMap<SmolStr, usize> {
+    let mut counts = HashMap::new();
     for n in h.nodes() {
         if let Some(eop) = h.get_optype(n).as_extension_op() {
             if [TKET_EXTENSION_ID, TKET1_EXTENSION_ID].contains(eop.extension_id()) {
-                if ["tket.quantum.QFree", "tket.quantum.QAlloc"]
-                    .contains(&eop.qualified_id().as_str())
-                {
-                    alloc += 1;
-                } else {
-                    non_alloc += 1;
-                }
+                *counts.entry(eop.qualified_id()).or_default() += 1;
             }
         }
     }
-    (non_alloc, alloc)
+    counts
 }
 
 /// Run some simple optimization passes on the guppy-generated HUGRs and validate the result.
@@ -69,15 +64,31 @@ fn count_gates(h: &impl HugrView) -> (usize, usize) {
 /// This test is intended to check the current status of the Guppy optimization passes.
 ///
 #[rstest]
-#[case::angles(guppy_angles(), (5, 1))]
-#[case::false_branch(guppy_false_branch(), (3, 1))]
-#[case::nested(guppy_nested(), (6, 3))]
-#[case::ranges(guppy_ranges(), (8, 4))]
-#[case::simple_cx(guppy_simple_cx(), (4, 2))]
+#[case::angles(guppy_angles(), [
+    ("tket.quantum.H", 2), ("tket.quantum.QAlloc", 1), ("tket.quantum.Rz", 2), ("tket.quantum.MeasureFree", 1)
+])]
+#[case::false_branch(guppy_false_branch(), [
+    ("tket.quantum.H", 2), ("tket.quantum.QAlloc", 1), ("tket.quantum.MeasureFree", 1)
+])]
+#[case::nested(guppy_nested(), [
+    ("tket.quantum.CZ", 1), ("tket.quantum.H", 2), ("tket.quantum.QAlloc", 3), ("tket.quantum.MeasureFree", 3)
+])]
+#[case::ranges(guppy_ranges(), [
+    ("tket.quantum.QAlloc", 4), ("tket.quantum.MeasureFree", 4), ("tket.quantum.H", 2), ("tket.quantum.CX", 2)
+])]
+#[case::simple_cx(guppy_simple_cx(), [
+    ("tket.quantum.QAlloc", 2), ("tket.quantum.CX", 2), ("tket.quantum.MeasureFree", 2)
+])]
 #[cfg_attr(miri, ignore)] // Opening files is not supported in (isolated) miri
-fn optimise_guppy(#[case] mut hugr: Hugr, #[case] before: (usize, usize)) {
+fn optimise_guppy(
+    #[case] mut hugr: Hugr,
+    #[case] before: impl IntoIterator<Item = (&'static str, usize)>,
+) {
     NormalizeGuppy::default().run(&mut hugr).unwrap();
-    assert_eq!(count_gates(&hugr), before);
+    assert_eq!(
+        count_gates(&hugr),
+        before.into_iter().map(|(k, v)| (k.into(), v)).collect()
+    );
 
     // TODO: Run pytket passes here, and check that the circuit is as optimized as possible at this point.
     //
