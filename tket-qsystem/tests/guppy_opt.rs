94164a640584d11558fc6c7097be95eabc21bf03
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2025-11-19 15:33:01 +0000

    Call NormalizeGuppy in optimize_flatted_guppy

--- tket-qsystem/tests/guppy_opt.rs
+++ tket-qsystem/tests/guppy_opt.rs
@@ -78,6 +78,7 @@ fn count_gates(h: &impl HugrView) -> HashMap<SmolStr, usize> {
 ///
 
 #[rstest]
+#[case::nested_array("nested_array", None)]
 #[should_panic = "xfail"]
 #[case::angles("angles", Some(vec![
     ("tket.quantum.Rz", 2), ("tket.quantum.MeasureFree", 1), ("tket.quantum.H", 2), ("tket.quantum.QAlloc", 1)
@@ -96,14 +97,15 @@ fn count_gates(h: &impl HugrView) -> HashMap<SmolStr, usize> {
 ]))]
 #[should_panic = "xfail"]
 #[case::false_branch("false_branch", Some(vec![
-    ("TKET1.tk1op", 2), ("tket.quantum.QAlloc", 1), ("tket.quantum.MeasureFree", 1)
+    ("TKET1.tk1op", 1), ("tket.quantum.H", 1), ("tket.quantum.QAlloc", 1), ("tket.quantum.MeasureFree", 1)
 ]))]
-//nested_array works (see `optimize_guppy` below), but only with NormalizeGuppy (BorrowSquash),
-//and there is no easy way to express that in guppy
 #[cfg_attr(miri, ignore)] // Opening files is not supported in (isolated) miri
 fn optimize_flattened_guppy(#[case] name: &str, #[case] xfail: Option<Vec<(&str, usize)>>) {
     let mut hugr = load_guppy_circuit(name, HugrFileType::Flat)
         .unwrap_or_else(|_| load_guppy_circuit(name, HugrFileType::Original).unwrap());
+    // We don't need NormalizeGuppy to "flatten" control flow here, but we still want
+    // to get rid of other guppy artifacts.
+    NormalizeGuppy::default().run(&mut hugr).unwrap();
     run_pytket(&mut hugr);
     let should_xfail = xfail.is_some();
     let expected_counts = match xfail {
